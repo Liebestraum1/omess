2dddc584b0853a669c6ad643ad452fa07ed04116
JuHyeong <bndgc@naver.com>
2024-04-18 16:14:46 +0900

    Init: 프로젝트 생성

---
+++ back/omess/src/main/java/org/sixback/omess/OmessApplication.java
@@ -0,0 +1,13 @@
+package org.sixback.omess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmessApplication.class, args);
+    }
+
+}
