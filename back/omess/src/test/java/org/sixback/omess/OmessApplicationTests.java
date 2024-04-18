0c5cb51e55b70d45a7df7777670a909f7ae786d6
JuHyeong <bndgc@naver.com>
2024-04-18 16:14:46 +0900

    Init: 프로젝트 생성

---
+++ back/omess/src/test/java/org/sixback/omess/OmessApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sixback.omess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
