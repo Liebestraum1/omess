f41d75eff79900ac97eb3abf8eb2e9e8c020fbf3
jsy6265 <wjs6265@gmail.com>
2024-04-21 17:03:27 +0900

    Feat : KanbanBoard

--- back/src/main/java/org/sixback/omess/domain/member/model/entity/Member.java
+++ back/src/main/java/org/sixback/omess/domain/member/model/entity/Member.java
@@ -1,6 +1,5 @@
 package org.sixback.omess.domain.member.model.entity;
 
-<<<<<<< Updated upstream
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
