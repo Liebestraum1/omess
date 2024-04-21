9d215bf2c661af9b2c12399859c4ef503821316d
bndgc@naver.com <bndgc@naver.com>
2024-04-21 15:48:09 +0900

    Add: Project entity

---
+++ back/src/main/java/org/sixback/omess/domain/project/model/entity/Project.java
@@ -0,0 +1,24 @@
+package org.sixback.omess.domain.project.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sixback.omess.common.BaseTimeEntity;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Project extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+}
