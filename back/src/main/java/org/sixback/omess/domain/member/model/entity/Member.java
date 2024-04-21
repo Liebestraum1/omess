cb12a33621e4f6b89b9265e41e11edfa0b1893a3
bndgc@naver.com <bndgc@naver.com>
2024-04-21 15:32:42 +0900

    Add: Member entity

---
+++ back/src/main/java/org/sixback/omess/domain/member/model/entity/Member.java
@@ -0,0 +1,27 @@
+package org.sixback.omess.domain.member.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sixback.omess.common.BaseTimeEntity;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Member extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    @Column(length = 10, unique = true, nullable = false)
+    private String nickname;
+    @Column(length = 20, unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+}
