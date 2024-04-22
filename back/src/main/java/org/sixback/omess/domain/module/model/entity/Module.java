06edaa3e6f4f71b3ae15c34f008f14eed99f0ae6
jsy6265 <71249591+jsy6265@users.noreply.github.com>
2024-04-22 10:14:41 +0900

    feat : add module category field

--- back/src/main/java/org/sixback/omess/domain/module/model/entity/Module.java
+++ back/src/main/java/org/sixback/omess/domain/module/model/entity/Module.java
@@ -20,11 +20,15 @@ public class Module extends BaseTimeEntity {
     @Column(length = 50, nullable = false)
     private String title;
 
+    @Column(length = 50, nullable = false)
+    private String category;
+
     @ManyToOne(fetch = FetchType.LAZY)
     private Project project;
 
-    public Module(String title, Project project){
+    public Module(String title, String category, Project project){
         this.title = title;
+        this.category = category;
         this.project = project;
     }
 }
