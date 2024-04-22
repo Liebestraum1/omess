388749a9b7ae3164ffdb37c58c5a9f04cbce3a7c
jsy6265 <71249591+jsy6265@users.noreply.github.com>
2024-04-22 15:07:35 +0900

    feat : add getModules, updateModule, getModuleCategories

--- back/src/main/java/org/sixback/omess/domain/module/controller/ModuleController.java
+++ back/src/main/java/org/sixback/omess/domain/module/controller/ModuleController.java
@@ -1,13 +1,32 @@
 package org.sixback.omess.domain.module.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.sixback.omess.domain.module.model.dto.response.GetModuleCategoryResponse;
+import org.sixback.omess.domain.module.model.dto.response.GetModuleResponse;
 import org.sixback.omess.domain.module.service.ModuleService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/modules")
 @RequiredArgsConstructor
 public class ModuleController {
-//    private final ModuleService moduleService;
+    private final ModuleService moduleService;
+
+    @GetMapping("/project_id")
+    public List<GetModuleResponse> getModules(@PathVariable("project_id")Long projectId){
+        return moduleService.getModules(projectId);
+    }
+
+    @PatchMapping("/module_id")
+    public void updateModule(@PathVariable("module_id")Long module_id, @RequestBody String title){
+        moduleService.updateModule(module_id, title);
+    }
+
+    @GetMapping
+    public List<GetModuleCategoryResponse> getModuleCategories(){
+        return moduleService.getModuleCategories();
+    }
+
 }
