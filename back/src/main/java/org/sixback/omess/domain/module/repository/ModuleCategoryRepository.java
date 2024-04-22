4dc22323bc22f907da78c94b78029d2666da8394
jsy6265 <71249591+jsy6265@users.noreply.github.com>
2024-04-22 15:07:00 +0900

    feat : add ModuleCategoryRepository

---
+++ back/src/main/java/org/sixback/omess/domain/module/repository/ModuleCategoryRepository.java
@@ -0,0 +1,7 @@
+package org.sixback.omess.domain.module.repository;
+
+import org.sixback.omess.domain.module.model.entity.ModuleCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ModuleCategoryRepository extends JpaRepository<ModuleCategory, Long> {
+}
