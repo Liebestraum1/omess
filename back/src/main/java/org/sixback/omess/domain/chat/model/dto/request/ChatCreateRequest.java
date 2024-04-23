67ad2e72bc261ed62d36993ecc1d239e9c645b19
chlwodyd9829 <chlwodyd9829@naver.com>
2024-04-23 10:24:58 +0900

    add: 방 이름 필드 추가

--- back/src/main/java/org/sixback/omess/domain/chat/model/dto/request/ChatCreateRequest.java
+++ back/src/main/java/org/sixback/omess/domain/chat/model/dto/request/ChatCreateRequest.java
@@ -1,5 +1,6 @@
 package org.sixback.omess.domain.chat.model.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
@@ -9,4 +10,8 @@ import java.util.List;
 public class ChatCreateRequest {
     @Size(min = 2, message = "2명 이상을 선택해 주세요.")
     private List<String> emails;
+
+    @NotBlank
+    @Size(min = 1)
+    private String name;
 }
