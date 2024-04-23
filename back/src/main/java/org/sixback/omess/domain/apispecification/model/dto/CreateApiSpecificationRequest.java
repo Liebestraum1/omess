97241bb787f9a7d4fb98ebf701643d56c0d5e7b6
doongdang <doongdang@naver.com>
2024-04-23 15:58:14 +0900

    fix: projectId Path Variable로 변경

--- back/src/main/java/org/sixback/omess/domain/apispecification/model/dto/CreateApiSpecificationRequest.java
+++ back/src/main/java/org/sixback/omess/domain/apispecification/model/dto/CreateApiSpecificationRequest.java
@@ -1,16 +1,12 @@
 package org.sixback.omess.domain.apispecification.model.dto;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 
 public record CreateApiSpecificationRequest(
         @NotEmpty
         String name,
 
         @NotEmpty
-        String category,
-
-        @NotNull
-        Long projectId
+        String category
 ) {
 }
