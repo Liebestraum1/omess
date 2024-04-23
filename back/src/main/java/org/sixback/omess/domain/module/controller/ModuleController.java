438b2f063eea09ef991c8ba115aecfe684123f9d
jsy6265 <71249591+jsy6265@users.noreply.github.com>
2024-04-23 12:57:29 +0900

    feat : fix ResponseEntity

--- back/src/main/java/org/sixback/omess/domain/module/controller/ModuleController.java
+++ back/src/main/java/org/sixback/omess/domain/module/controller/ModuleController.java
@@ -1,12 +1,11 @@
 package org.sixback.omess.domain.module.controller;
 
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.sixback.omess.domain.module.model.dto.request.UpdateMouleRequest;
 import org.sixback.omess.domain.module.model.dto.response.GetModuleCategoryResponse;
 import org.sixback.omess.domain.module.model.dto.response.GetModuleResponse;
 import org.sixback.omess.domain.module.service.ModuleService;
-import org.sixback.omess.domain.project.repository.ProjectMemberRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,19 +16,25 @@ import java.util.List;
 public class ModuleController {
     private final ModuleService moduleService;
 
-    @GetMapping("/project_id")
-    public List<GetModuleResponse> getModules(@SessionAttribute(name = "memberId") Long memberId, @PathVariable("project_id") Long projectId) {
-        return moduleService.getModules(projectId, memberId);
+    @GetMapping("/{project_id}")
+    public ResponseEntity<List<GetModuleResponse>> getModules(
+            @SessionAttribute(name = "memberId") Long memberId,
+            @PathVariable("project_id") Long projectId) {
+        return ResponseEntity.ok().body(moduleService.getModules(projectId, memberId));
     }
 
-    @PatchMapping("/module_id")
-    public void updateModule(@SessionAttribute(name = "memberId") Long memberId, @PathVariable("module_id") Long moduleId, @RequestBody UpdateMouleRequest updateMouleRequest) {
+    @PatchMapping("/{module_id}")
+    public ResponseEntity<Void> updateModule(
+            @SessionAttribute(name = "memberId") Long memberId,
+            @PathVariable("module_id") Long moduleId,
+            @RequestBody UpdateMouleRequest updateMouleRequest) {
         moduleService.updateModule(memberId, moduleId, updateMouleRequest);
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping
-    public List<GetModuleCategoryResponse> getModuleCategories() {
-        return moduleService.getModuleCategories();
+    public ResponseEntity<List<GetModuleCategoryResponse>> getModuleCategories() {
+        return ResponseEntity.ok().body(moduleService.getModuleCategories());
     }
 
 }
