36f36a1e7edcdb840bedf41067d39ba7c56b9256
doongdang <doongdang@naver.com>
2024-04-23 16:21:53 +0900

    feat: BaseTimeEntity 추가

--- back/src/main/java/org/sixback/omess/domain/apispecification/model/entity/Domain.java
+++ back/src/main/java/org/sixback/omess/domain/apispecification/model/entity/Domain.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.sixback.omess.common.BaseTimeEntity;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
@@ -13,7 +14,7 @@ import static lombok.AccessLevel.PROTECTED;
 @Entity
 @Getter
 @NoArgsConstructor(access = PROTECTED)
-public class Domain {
+public class Domain extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
