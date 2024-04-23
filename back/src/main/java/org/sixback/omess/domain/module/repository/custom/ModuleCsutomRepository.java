3bce8b97b3a2d63d703b42f3fd829be1ce37b7d0
jsy6265 <71249591+jsy6265@users.noreply.github.com>
2024-04-23 12:59:20 +0900

    feat : fix findByProjectId

--- back/src/main/java/org/sixback/omess/domain/module/repository/custom/ModuleCsutomRepository.java
+++ back/src/main/java/org/sixback/omess/domain/module/repository/custom/ModuleCsutomRepository.java
@@ -5,5 +5,5 @@ import org.sixback.omess.domain.module.model.entity.Module;
 import java.util.List;
 
 public interface ModuleCsutomRepository  {
-    List<Module> findByProjectID(Long projectId);
+    List<Module> findByProjectId(Long projectId);
 }
