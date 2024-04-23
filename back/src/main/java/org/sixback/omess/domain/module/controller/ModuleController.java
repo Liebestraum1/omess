4d4509a1b9ce4c2f56f922a81f101b9ae63a4d19
jsy6265 <71249591+jsy6265@users.noreply.github.com>
2024-04-23 09:15:33 +0900

    feat : update module service, kanbanboard service validation project member

--- back/src/main/java/org/sixback/omess/domain/module/controller/ModuleController.java
+++ back/src/main/java/org/sixback/omess/domain/module/controller/ModuleController.java
@@ -1,9 +1,12 @@
 package org.sixback.omess.domain.module.controller;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.sixback.omess.domain.module.model.dto.request.UpdateMouleRequest;
 import org.sixback.omess.domain.module.model.dto.response.GetModuleCategoryResponse;
 import org.sixback.omess.domain.module.model.dto.response.GetModuleResponse;
 import org.sixback.omess.domain.module.service.ModuleService;
+import org.sixback.omess.domain.project.repository.ProjectMemberRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,17 +18,17 @@ public class ModuleController {
     private final ModuleService moduleService;
 
     @GetMapping("/project_id")
-    public List<GetModuleResponse> getModules(@PathVariable("project_id")Long projectId){
-        return moduleService.getModules(projectId);
+    public List<GetModuleResponse> getModules(@SessionAttribute(name = "memberId") Long memberId, @PathVariable("project_id") Long projectId) {
+        return moduleService.getModules(projectId, memberId);
     }
 
     @PatchMapping("/module_id")
-    public void updateModule(@PathVariable("module_id")Long moduleId, @RequestBody String title){
-        moduleService.updateModule(moduleId, title);
+    public void updateModule(@SessionAttribute(name = "memberId") Long memberId, @PathVariable("module_id") Long moduleId, @RequestBody UpdateMouleRequest updateMouleRequest) {
+        moduleService.updateModule(memberId, moduleId, updateMouleRequest);
     }
 
     @GetMapping
-    public List<GetModuleCategoryResponse> getModuleCategories(){
+    public List<GetModuleCategoryResponse> getModuleCategories() {
         return moduleService.getModuleCategories();
     }
 
