752b5549d4f4d94e9de1e60292fdd8f61f164381
doongdang <doongdang@naver.com>
2024-04-23 17:21:05 +0900

    feat: 테스트용 Project 생성

--- back/src/test/java/org/sixback/omess/common/TestUtils.java
+++ back/src/test/java/org/sixback/omess/common/TestUtils.java
@@ -1,6 +1,7 @@
 package org.sixback.omess.common;
 
 import org.sixback.omess.domain.member.model.entity.Member;
+import org.sixback.omess.domain.project.model.entity.Project;
 
 import static org.sixback.omess.common.RandomUtils.generateRandomEmail;
 import static org.sixback.omess.common.RandomUtils.generateRandomString;
@@ -12,5 +13,13 @@ public class TestUtils {
 
     public static Member makeMember(String nickname, String email, String password) {
         return new Member(nickname, email, password);
+    }
+
+    public static Project makeProject() {
+        return new Project(generateRandomString(10));
+    }
+
+    public static Project makeProject(String name) {
+        return new Project(name);
     }
 }
