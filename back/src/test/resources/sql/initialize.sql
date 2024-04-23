11e9cf5a3e4843561625adc4281e29af4dacabc2
doongdang <doongdang@naver.com>
2024-04-23 16:20:37 +0900

    chore: 기타 schema 추가 project_member, module, kanbanboard, label, issue, api_specification, domain, api,http_method 스키마 추가

--- back/src/test/resources/sql/initialize.sql
+++ back/src/test/resources/sql/initialize.sql
@@ -2,5 +2,14 @@ SET FOREIGN_KEY_CHECKS=0;
 
 TRUNCATE TABLE `member`;
 TRUNCATE TABLE `project`;
+TRUNCATE TABLE `project_member`;
+TRUNCATE TABLE `module`;
+TRUNCATE TABLE `kanbanboard`;
+TRUNCATE TABLE `label`;
+TRUNCATE TABLE `issue`;
+TRUNCATE TABLE `api_specification`;
+TRUNCATE TABLE `domain`;
+TRUNCATE TABLE `api`;
+TRUNCATE TABLE `http_method`;
 
 SET FOREIGN_KEY_CHECKS=1;
