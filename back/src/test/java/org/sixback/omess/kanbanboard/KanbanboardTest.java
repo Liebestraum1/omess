3bf93b307a91464d327649cb4b0f63d2f1f5855b
jsy6265 <71249591+jsy6265@users.noreply.github.com>
2024-04-23 12:59:47 +0900

    feat : update test

--- back/src/test/java/org/sixback/omess/kanbanboard/KanbanboardTest.java
+++ back/src/test/java/org/sixback/omess/kanbanboard/KanbanboardTest.java
@@ -5,8 +5,13 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.sixback.omess.domain.kanbanboard.model.dto.request.DeleteKanbanBoardRequest;
 import org.sixback.omess.domain.kanbanboard.model.dto.request.WriteKanbanBoardRequest;
+import org.sixback.omess.domain.kanbanboard.model.dto.response.GetKanbanBoardResponse;
+import org.sixback.omess.domain.kanbanboard.model.entity.Issue;
 import org.sixback.omess.domain.kanbanboard.model.entity.KanbanBoard;
+import org.sixback.omess.domain.kanbanboard.model.entity.Label;
+import org.sixback.omess.domain.kanbanboard.repository.IssueRepository;
 import org.sixback.omess.domain.kanbanboard.repository.KanbanBoardRepository;
+import org.sixback.omess.domain.kanbanboard.repository.LabelRepository;
 import org.sixback.omess.domain.kanbanboard.service.KanbanBoardService;
 import org.sixback.omess.domain.member.model.entity.Member;
 import org.sixback.omess.domain.member.repository.MemberRepository;
@@ -19,6 +24,7 @@ import org.sixback.omess.domain.project.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
@@ -44,6 +50,12 @@ public class KanbanboardTest {
 
     @Autowired
     ProjectMemberRepository projectMemberRepository;
+
+    @Autowired
+    IssueRepository issueRepository;
+
+    @Autowired
+    LabelRepository labelRepository;
 
     // 칸반보드 생성/삭제 테스트
     @Test
@@ -83,6 +95,66 @@ public class KanbanboardTest {
         List<Module> afterModules = moduleRepository.findAll();
 
         System.out.println("삭제 후 " + afterModules.size());
+    }
+
+    @Test
+    public void getKanbanBoard() {
+        Member member = new Member("bam", "wjs6265", "wb6265");
+
+        memberRepository.save(member);
+
+
+        Project project = new Project("프젝 1");
+
+        projectRepository.save(project);
+
+        ProjectMember projectMember = new ProjectMember(project, member);
+
+        projectMemberRepository.save(projectMember);
+
+        KanbanBoard kanbanBoard = new KanbanBoard("칸반보드", "KanbanBoard", project);
+        kanbanBoardRepository.save(kanbanBoard);
+
+        KanbanBoard kanbanBoard2 = new KanbanBoard("칸반보드 2", "KanbanBoard", project);
+        kanbanBoardRepository.save(kanbanBoard2);
+
+        List<Issue> issues = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            issues.add(new Issue("할일 " + i, "이거해라", 0, 0, kanbanBoard));
+        }
+
+        for (int i = 0; i < 10; i++) {
+            issues.add(new Issue("할일 " + i, "이거해라", 0, 0, kanbanBoard2));
+        }
+
+        issueRepository.saveAll(issues);
+
+        List<Label> labels = new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+            if (i % 2 == 0) {
+                Label label = new Label("라벨 " + i);
+                labels.add(label);
+                labelRepository.save(label);
+
+                issues.get(i).updateLabel(label);
+            }
+        }
+
+        em.flush();
+
+        GetKanbanBoardResponse getKanbanBoardResponse = kanbanBoardService.getKanbanBoard(member.getId(), kanbanBoard.getId(), project.getId());
+        System.out.println(getKanbanBoardResponse.title());
+        System.out.println(getKanbanBoardResponse.issues().size());
+
+        for (int i = 0; i < getKanbanBoardResponse.issues().size(); i++) {
+            if (getKanbanBoardResponse.issues().get(i).label() != null) {
+                System.out.println("이슈 : " + getKanbanBoardResponse.issues().get(i).title() + " 라벨 : " + getKanbanBoardResponse.issues().get(i).label().name());
+            }else{
+                System.out.println("이슈 : " + getKanbanBoardResponse.issues().get(i).title() + " 라벨 : " + "없음");
+            }
+        }
     }
 
 }
