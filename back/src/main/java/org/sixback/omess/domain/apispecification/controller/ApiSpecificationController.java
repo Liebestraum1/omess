6db6ad22db17ac84f111b17cd506efc0ba54b1f4
doongdang <doongdang@naver.com>
2024-04-23 17:39:34 +0900

    feat: @Valid

--- back/src/main/java/org/sixback/omess/domain/apispecification/controller/ApiSpecificationController.java
+++ back/src/main/java/org/sixback/omess/domain/apispecification/controller/ApiSpecificationController.java
@@ -1,5 +1,6 @@
 package org.sixback.omess.domain.apispecification.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.sixback.omess.domain.apispecification.model.dto.CreateApiSpecificationRequest;
 import org.sixback.omess.domain.apispecification.service.ApiSpecificationService;
@@ -16,7 +17,7 @@ public class ApiSpecificationController {
     public ResponseEntity<Void> addApiSpecification(
             @SessionAttribute(name = "memberId") Long memberId, //FIXME : 추후 Project에 속한 Member인지 확인하는 AOP or Filter로 대체
             @PathVariable Long projectId,
-            @RequestBody CreateApiSpecificationRequest createApiSpecificationRequest
+            @Valid @RequestBody CreateApiSpecificationRequest createApiSpecificationRequest
     ) {
         apiSpecificationService.createApiSpecification(memberId, projectId, createApiSpecificationRequest);
         return ResponseEntity.ok().build();
