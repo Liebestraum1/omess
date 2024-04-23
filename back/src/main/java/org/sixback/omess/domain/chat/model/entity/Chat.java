b28c5725b53a87fc77511fb654f2b84471f52b4d
chlwodyd9829 <chlwodyd9829@naver.com>
2024-04-23 10:23:21 +0900

    add: Setter 추가

--- back/src/main/java/org/sixback/omess/domain/chat/model/entity/Chat.java
+++ back/src/main/java/org/sixback/omess/domain/chat/model/entity/Chat.java
@@ -3,6 +3,7 @@ package org.sixback.omess.domain.chat.model.entity;
 import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
@@ -16,8 +17,11 @@ public class Chat {
     @MongoId
     private String chatId;
     private Long projectId;
+    @Setter
     private String name;
+    @Setter
     private Content header;
+    @Setter
     private Content notice;
     private List<ChatMember> members;
     private List<Content> messages = new ArrayList<>();
