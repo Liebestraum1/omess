8465d99231a25ea4e2e736ae59ad9c12669529f5
jsy6265 <71249591+jsy6265@users.noreply.github.com>
2024-04-24 17:54:03 +0900

    feat : add querydslconfig

--- back/src/main/java/org/sixback/omess/domain/module/repository/custom/ModuleRepositoryImpl.java
+++ back/src/main/java/org/sixback/omess/domain/module/repository/custom/ModuleRepositoryImpl.java
@@ -1,20 +1,17 @@
 package org.sixback.omess.domain.module.repository.custom;
 
-import com.querydsl.jpa.JPQLQueryFactory;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.sixback.omess.domain.module.model.entity.Module;
 import org.sixback.omess.domain.module.model.entity.QModule;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
+@RequiredArgsConstructor
 public class ModuleRepositoryImpl implements ModuleCsutomRepository{
-    private final JPQLQueryFactory jpaQueryFactory;
-
-    public ModuleRepositoryImpl(EntityManager entityManager) {
-        this.jpaQueryFactory = new JPAQueryFactory(entityManager);
-    }
+    private final JPAQueryFactory jpaQueryFactory;
     private final QModule qModule = QModule.module;
     @Override
     public List<Module> findByProjectId(Long projectId) {
