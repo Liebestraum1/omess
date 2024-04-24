157904decdd92ee129b03e6c184774f08c1eb2c4
jsy6265 <71249591+jsy6265@users.noreply.github.com>
2024-04-24 17:56:09 +0900

    feat : add configscan, enablejpaauto

--- back/src/main/java/org/sixback/omess/OmessApplication.java
+++ back/src/main/java/org/sixback/omess/OmessApplication.java
@@ -3,9 +3,11 @@ package org.sixback.omess;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class OmessApplication {
 
     public static void main(String[] args) {
