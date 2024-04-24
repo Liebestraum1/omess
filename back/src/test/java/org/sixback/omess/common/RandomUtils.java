c49e44812222e826cea0b1c86b9c7ea6a922c045
chlwodyd9829 <chlwodyd9829@naver.com>
2024-04-24 15:10:24 +0900

    feat: 랜덤 숫자 생성 기능

--- back/src/test/java/org/sixback/omess/common/RandomUtils.java
+++ back/src/test/java/org/sixback/omess/common/RandomUtils.java
@@ -2,6 +2,8 @@ package org.sixback.omess.common;
 
 import net.bytebuddy.utility.RandomString;
 
+import java.util.Random;
+
 public class RandomUtils {
     public static String generateRandomString(int length) {
         return RandomString.make(length);
@@ -11,5 +13,11 @@ public class RandomUtils {
         String userName = generateRandomString(10); // 위의 랜덤 문자열 생성 메서드 사용
         String domainName = generateRandomString(5);
         return userName + "@" + domainName + ".com";
+    }
+
+    public static int generateRandomNumber(int limit) {
+        int start = limit == 1 ? 0 : 1;
+        Random random = new Random();
+        return random.nextInt(start, limit);
     }
 }
