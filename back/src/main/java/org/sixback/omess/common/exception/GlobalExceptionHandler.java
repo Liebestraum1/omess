e590cff5bea073c48b7a69d955aa698646f4908f
bndgc@naver.com <bndgc@naver.com>
2024-04-25 12:01:52 +0900

    refactor: globalException message enum으로 변경

--- back/src/main/java/org/sixback/omess/common/exception/GlobalExceptionHandler.java
+++ back/src/main/java/org/sixback/omess/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package org.sixback.omess.common.exception;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,21 +13,35 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
 import java.net.URI;
+import java.util.List;
 
+import static org.sixback.omess.common.exception.ErrorType.*;
 import static org.springframework.http.HttpStatus.*;
 
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ErrorResponse httpMessageNotReadableExceptionException(
+            HttpServletRequest request, HttpMessageNotReadableException exception
+    ) {
+        printException(exception);
+        return ErrorResponse.builder(exception, BAD_REQUEST, INCOMPLETE_REQUEST_BODY_ERROR.getTitle())
+                .type(URI.create(VALIDATION_ERROR.name()))
+                .title(INCOMPLETE_REQUEST_BODY_ERROR.getTitle())
+                .instance(URI.create(request.getRequestURI()))
+                .build();
+    }
+
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public ErrorResponse missingServletRequestParameterException(
             HttpServletRequest request, MissingServletRequestParameterException exception
     ) {
         printException(exception);
-        return ErrorResponse.builder(exception, BAD_REQUEST, exception.getParameterName() + " 은 필수 값입니다.")
-                .type(URI.create("VALIDATION_ERROR"))
-                .title("유효한 요청이 아닙니다.")
+        return ErrorResponse.builder(exception, BAD_REQUEST, "parameter: " + exception.getParameterName() + " 은 필수 값입니다.")
+                .type(URI.create(VALIDATION_ERROR.name()))
+                .title(VALIDATION_ERROR.getTitle())
                 .instance(URI.create(request.getRequestURI()))
                 .build();
     }
@@ -38,8 +53,8 @@ public class GlobalExceptionHandler {
         printException(exception);
         String detail = exception.getAllValidationResults().toString();
         return ErrorResponse.builder(exception, BAD_REQUEST, detail)
-                .type(URI.create("VALIDATION_ERROR"))
-                .title("유효한 요청이 아닙니다.")
+                .type(URI.create(VALIDATION_ERROR.name()))
+                .title(VALIDATION_ERROR.getTitle())
                 .instance(URI.create(request.getRequestURI()))
                 .build();
     }
@@ -49,10 +64,18 @@ public class GlobalExceptionHandler {
             HttpServletRequest request, BindException exception
     ) {
         printException(exception);
-        String detail = exception.getBindingResult().getFieldErrors().toString();
-        return ErrorResponse.builder(exception, BAD_REQUEST, detail)
-                .type(URI.create("VALIDATION_ERROR"))
-                .title("유효한 요청이 아닙니다")
+        List<FieldErrorMessage> detail = exception.getBindingResult()
+                .getFieldErrors()
+                .stream().map(FieldErrorMessage::new)
+                .toList();
+//        StringBuilder detail = new StringBuilder();
+//        for (FieldErrorMessage fieldErrorMessage : fieldErrorMessages) {
+//            detail.append(fieldErrorMessage)
+//                    .append('\n');
+//        }
+        return ErrorResponse.builder(exception, BAD_REQUEST, detail.toString())
+                .type(URI.create(VALIDATION_ERROR.name()))
+                .title(VALIDATION_ERROR.getTitle())
                 .instance(URI.create(request.getRequestURI()))
                 .build();
     }
@@ -62,9 +85,9 @@ public class GlobalExceptionHandler {
             HttpServletRequest request, EntityNotFoundException exception
     ) {
         printException(exception);
-        return ErrorResponse.builder(exception, NOT_FOUND, "요청한 data를 찾을 수 없습니다.")
-                .type(URI.create("NOT_FOUND"))
-                .title("data를 찾을 수 없습니다.")
+        return ErrorResponse.builder(exception, NOT_FOUND, NOT_FOUND_ERROR.getTitle())
+                .type(URI.create(NOT_FOUND_ERROR.name()))
+                .title(NOT_FOUND_ERROR.getTitle())
                 .instance(URI.create(request.getRequestURI()))
                 .build();
     }
@@ -75,13 +98,13 @@ public class GlobalExceptionHandler {
     ) {
         printException(exception);
         return ErrorResponse.builder(exception, INTERNAL_SERVER_ERROR, "서버에 알 수 없는 에러가 발생여였습니다.")
-                .type(URI.create("UNKNOW_ERROR"))
-                .title("알 수 없는 에러")
+                .type(URI.create(UNKNOWN_ERROR.name()))
+                .title(UNKNOWN_ERROR.getTitle())
                 .instance(URI.create(request.getRequestURI()))
                 .build();
     }
 
     private void printException(Exception exception) {
-        log.error("{} 발생 - {}", exception.getClass(), exception.getMessage());
+        log.error("{} 발생: {}", exception.getClass(), exception.getMessage());
     }
 }
