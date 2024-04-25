c6e7f5ebf4986bfa000e25397205384fd766c617
jsy6265 <71249591+jsy6265@users.noreply.github.com>
2024-04-25 16:35:46 +0900

    feat : add issue update, get issue

--- back/src/main/java/org/sixback/omess/domain/kanbanboard/controller/KanbanBoardController.java
+++ back/src/main/java/org/sixback/omess/domain/kanbanboard/controller/KanbanBoardController.java
@@ -1,11 +1,13 @@
 package org.sixback.omess.domain.kanbanboard.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.sixback.omess.domain.kanbanboard.model.dto.request.UpdateIssueRequest;
-import org.sixback.omess.domain.kanbanboard.model.dto.request.WriteIssueRequest;
-import org.sixback.omess.domain.kanbanboard.model.dto.request.WriteKanbanBoardRequest;
-import org.sixback.omess.domain.kanbanboard.model.dto.response.GetKanbanBoardResponse;
+import org.sixback.omess.domain.kanbanboard.model.dto.request.issue.UpdateIssueRequest;
+import org.sixback.omess.domain.kanbanboard.model.dto.request.issue.WriteIssueRequest;
+import org.sixback.omess.domain.kanbanboard.model.dto.request.kanbanboard.WriteKanbanBoardRequest;
+import org.sixback.omess.domain.kanbanboard.model.dto.response.issue.GetIssueDetailResponse;
+import org.sixback.omess.domain.kanbanboard.model.dto.response.kanbanboard.GetKanbanBoardResponse;
 import org.sixback.omess.domain.kanbanboard.service.KanbanBoardService;
+import org.sixback.omess.domain.member.model.dto.response.GetIssueResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -53,7 +55,7 @@ public class KanbanBoardController {
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/{module_id}/issue/{issue_id}")
+    @DeleteMapping("/{module_id}/issues/{issue_id}")
     public ResponseEntity<Void> deleteIssue(@SessionAttribute(name = "memberId") Long memberId,
                                             @PathVariable("project_id") Long projectId,
                                             @PathVariable("module_id") Long moduleId,
@@ -64,7 +66,7 @@ public class KanbanBoardController {
         return ResponseEntity.ok().build();
     }
 
-    @PatchMapping()
+    @PatchMapping("/{module_id}/issues/{issue_id}")
     public ResponseEntity<Void> updateIssue(@SessionAttribute(name = "memberId") Long memberId,
                                             @PathVariable("project_id") Long projectId,
                                             @PathVariable("module_id") Long moduleId,
@@ -75,4 +77,77 @@ public class KanbanBoardController {
 
         return ResponseEntity.ok().build();
     }
+
+    @PatchMapping("/{module_id}/issues/{issue_id}/member")
+    public ResponseEntity<Void> updateIssueMember(@SessionAttribute(name = "memberId") Long memberId,
+                                                  @PathVariable("project_id") Long projectId,
+                                                  @PathVariable("module_id") Long moduleId,
+                                                  @PathVariable("issue_id") Long issue_id,
+                                                  @RequestBody Long chargerId) {
+
+        kanbanBoardService.updateIssueMember(memberId, projectId, moduleId, issue_id, chargerId);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/{module_id}/issues/{issue_id}/importance")
+    public ResponseEntity<Void> updateIssueImportance(@SessionAttribute(name = "memberId") Long memberId,
+                                                      @PathVariable("project_id") Long projectId,
+                                                      @PathVariable("module_id") Long moduleId,
+                                                      @PathVariable("issue_id") Long issue_id,
+                                                      @RequestBody Integer importance) {
+
+        kanbanBoardService.updateIssueImportance(memberId, projectId, moduleId, issue_id, importance);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/{module_id}/issues/{issue_id}/status")
+    public ResponseEntity<Void> updateIssueStatus(@SessionAttribute(name = "memberId") Long memberId,
+                                                  @PathVariable("project_id") Long projectId,
+                                                  @PathVariable("module_id") Long moduleId,
+                                                  @PathVariable("issue_id") Long issue_id,
+                                                  @RequestBody Integer status) {
+
+        kanbanBoardService.updateIssueStatus(memberId, projectId, moduleId, issue_id, status);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/{module_id}/issues/{issue_id}/label")
+    public ResponseEntity<Void> updateIssueLabel(@SessionAttribute(name = "memberId") Long memberId,
+                                                 @PathVariable("project_id") Long projectId,
+                                                 @PathVariable("module_id") Long moduleId,
+                                                 @PathVariable("issue_id") Long issue_id,
+                                                 @RequestBody Long labelId) {
+
+        kanbanBoardService.updateIssueLabel(memberId, projectId, moduleId, issue_id, labelId);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{module_id}/issues")
+    public ResponseEntity<GetIssueResponses> getIssues(@SessionAttribute(name = "memberId") Long memberId,
+                                                       @PathVariable("project_id") Long projectId,
+                                                       @PathVariable("module_id") Long moduleId,
+                                                       @RequestParam(required = false) Long chargerId,
+                                                       @RequestParam(required = false) Long labelId,
+                                                       @RequestParam(required = false) Integer importance) {
+
+        GetIssueResponses getIssueResponses =  kanbanBoardService.getIssues(memberId, projectId, moduleId, chargerId, labelId, importance);
+
+        return ResponseEntity.ok().body(getIssueResponses);
+    }
+
+    @GetMapping("/{module_id}/issues/{issue_id}")
+    public ResponseEntity<GetIssueDetailResponse> getIssue(@SessionAttribute(name = "memberId") Long memberId,
+                                                           @PathVariable("project_id") Long projectId,
+                                                           @PathVariable("module_id") Long moduleId,
+                                                           @PathVariable("issue_id") Long issue_id){
+
+        GetIssueDetailResponse getIssueDetailResponse = kanbanBoardService.getIssue(memberId, projectId, moduleId, issue_id);
+
+        return ResponseEntity.ok().body(getIssueDetailResponse);
+    }
+
 }
