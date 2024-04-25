dd1819b7551c3f4a35c4746c8173174ae5dd1965
jsy6265 <71249591+jsy6265@users.noreply.github.com>
2024-04-25 16:34:56 +0900

    feat : add get issues

--- back/src/main/java/org/sixback/omess/domain/kanbanboard/repository/custom/IssueCustomRepository.java
+++ back/src/main/java/org/sixback/omess/domain/kanbanboard/repository/custom/IssueCustomRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface IssueCustomRepository {
     List<Issue> findByModuleId(Long moduleId);
 
+    List<Issue> getIssues(String path, Long chargerId, Long labelId, Integer importance);
 }
