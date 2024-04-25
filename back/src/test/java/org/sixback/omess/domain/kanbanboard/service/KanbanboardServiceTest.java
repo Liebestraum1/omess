d7548b14b033437bf1a7d7223115ab58eca82314
jsy6265 <71249591+jsy6265@users.noreply.github.com>
2024-04-25 16:32:49 +0900

    feat : add issue update test, issue get test

--- back/src/test/java/org/sixback/omess/domain/kanbanboard/service/KanbanboardServiceTest.java
+++ back/src/test/java/org/sixback/omess/domain/kanbanboard/service/KanbanboardServiceTest.java
@@ -4,17 +4,19 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
-import org.sixback.omess.domain.kanbanboard.model.dto.request.DeleteKanbanBoardRequest;
-import org.sixback.omess.domain.kanbanboard.model.dto.request.UpdateIssueRequest;
-import org.sixback.omess.domain.kanbanboard.model.dto.request.WriteIssueRequest;
-import org.sixback.omess.domain.kanbanboard.model.dto.request.WriteKanbanBoardRequest;
-import org.sixback.omess.domain.kanbanboard.model.dto.response.GetKanbanBoardResponse;
+import org.sixback.omess.domain.kanbanboard.model.dto.request.issue.UpdateIssueRequest;
+import org.sixback.omess.domain.kanbanboard.model.dto.request.issue.WriteIssueRequest;
+import org.sixback.omess.domain.kanbanboard.model.dto.request.kanbanboard.WriteKanbanBoardRequest;
+import org.sixback.omess.domain.kanbanboard.model.dto.response.issue.GetIssueDetailResponse;
+import org.sixback.omess.domain.kanbanboard.model.dto.response.issue.GetIssueResponse;
+import org.sixback.omess.domain.kanbanboard.model.dto.response.kanbanboard.GetKanbanBoardResponse;
 import org.sixback.omess.domain.kanbanboard.model.entity.Issue;
 import org.sixback.omess.domain.kanbanboard.model.entity.KanbanBoard;
 import org.sixback.omess.domain.kanbanboard.model.entity.Label;
 import org.sixback.omess.domain.kanbanboard.repository.IssueRepository;
 import org.sixback.omess.domain.kanbanboard.repository.KanbanBoardRepository;
 import org.sixback.omess.domain.kanbanboard.repository.LabelRepository;
+import org.sixback.omess.domain.member.model.dto.response.GetIssueResponses;
 import org.sixback.omess.domain.member.model.entity.Member;
 import org.sixback.omess.domain.member.repository.MemberRepository;
 import org.sixback.omess.domain.module.model.entity.Module;
@@ -91,8 +93,7 @@ public class KanbanboardServiceTest {
         for (int i = 0; i < kanbanBoards.size(); i++) {
             KanbanBoard findKanban = kanbanBoards.get(i);
             System.out.println(findKanban.getTitle());
-            DeleteKanbanBoardRequest deleteKanbanBoardRequest = new DeleteKanbanBoardRequest();
-            deleteKanbanBoardRequest.setProjectId(project.getId());
+
             kanbanBoardService.deleteKanbanBoard(member.getId(), findKanban.getId(), project.getId());
         }
 
@@ -118,18 +119,22 @@ public class KanbanboardServiceTest {
 
         KanbanBoard kanbanBoard = new KanbanBoard("칸반보드", "KanbanBoard", project);
         kanbanBoardRepository.save(kanbanBoard);
+        String k1Path = "P" + project.getId() + "/K" + kanbanBoard.getId();
+        kanbanBoard.updatePath(k1Path);
 
         KanbanBoard kanbanBoard2 = new KanbanBoard("칸반보드 2", "KanbanBoard", project);
         kanbanBoardRepository.save(kanbanBoard2);
+        String k2Path = "P" + project.getId() + "/K" + kanbanBoard2.getId();
+        kanbanBoard2.updatePath(k2Path);
 
         List<Issue> issues = new ArrayList<>();
 
         for (int i = 0; i < 10; i++) {
-            issues.add(new Issue("할일 " + i, "이거해라", 0, 0, kanbanBoard, null, null));
+            issues.add(new Issue("할일 " + i, "이거해라", 1, 1, kanbanBoard, null, null));
         }
 
         for (int i = 0; i < 10; i++) {
-            issues.add(new Issue("할일 " + i, "이거해라", 0, 0, kanbanBoard2, null, null));
+            issues.add(new Issue("할일 " + i, "이거해라", 1, 1, kanbanBoard2, null, null));
         }
 
         issueRepository.saveAll(issues);
@@ -141,6 +146,9 @@ public class KanbanboardServiceTest {
                 Label label = new Label("라벨 " + i, kanbanBoard);
                 labels.add(label);
                 labelRepository.save(label);
+
+                String lPath = k1Path + "/L" + label.getId();
+                label.updatePath(lPath);
 
                 issues.get(i).updateLabel(label);
             }
@@ -189,6 +197,8 @@ public class KanbanboardServiceTest {
         WriteIssueRequest writeIssueRequest = new WriteIssueRequest();
         writeIssueRequest.setTitle("이슈생성 테스트");
         writeIssueRequest.setContent("이슈생성 테스트 내용");
+        writeIssueRequest.setStatus(1);
+        writeIssueRequest.setImportance(1);
 
         kanbanBoardService.createIssue(project.getId(), kanbanBoard.getId(), writeIssueRequest);
 
@@ -218,21 +228,33 @@ public class KanbanboardServiceTest {
 
         KanbanBoard kanbanBoard = new KanbanBoard("칸반보드", "KanbanBoard", project);
         kanbanBoardRepository.save(kanbanBoard);
+        String k1Path = "P" + project.getId() + "/K" + kanbanBoard.getId();
+        kanbanBoard.updatePath(k1Path);
 
         KanbanBoard kanbanBoard2 = new KanbanBoard("칸반보드 2", "KanbanBoard", project);
         kanbanBoardRepository.save(kanbanBoard2);
+        String k2Path = "P" + project.getId() + "/K" + kanbanBoard2.getId();
+        kanbanBoard.updatePath(k2Path);
 
         List<Issue> issues = new ArrayList<>();
 
         for (int i = 0; i < 10; i++) {
-            issues.add(new Issue("할일 " + i, "이거해라", 0, 0, kanbanBoard, null, null));
+            Issue issue = new Issue("할일 " + i, "이거해라", 1, 1, kanbanBoard, null, null);
+            issueRepository.save(issue);
+            String iPath = k1Path + "/I" + issue.getId();
+            issue.updatePath(iPath);
+
+            issues.add(issue);
         }
 
         for (int i = 0; i < 10; i++) {
-            issues.add(new Issue("할일 " + i, "이거해라", 0, 0, kanbanBoard2, null, null));
-        }
+            Issue issue = new Issue("할일 " + i, "이거해라", 1, 1, kanbanBoard2, null, null);
+            issueRepository.save(issue);
+            String iPath = k2Path + "/I" + issue.getId();
+            issue.updatePath(iPath);
 
-        issueRepository.saveAll(issues);
+            issues.add(issue);
+        }
 
         List<Label> labels = new ArrayList<>();
 
@@ -276,10 +298,16 @@ public class KanbanboardServiceTest {
 
         KanbanBoard kanbanBoard = new KanbanBoard("칸반보드", "KanbanBoard", project);
         kanbanBoardRepository.save(kanbanBoard);
+
+        String kPath = "P" + project.getId() + "/K" + kanbanBoard.getId();
+        kanbanBoard.updatePath(kPath);
         
-        Issue issue = new Issue("이슈 테스트", "이슈 테스트 입니다.", 0, 0, kanbanBoard, null, null);
+        Issue issue = new Issue("이슈 테스트", "이슈 테스트 입니다.", 1, 1, kanbanBoard, null, null);
         
         issueRepository.save(issue);
+
+        String iPath = kPath + "/I" + issue.getId();
+        issue.updatePath(iPath);
 
         UpdateIssueRequest updateIssueRequest = new UpdateIssueRequest();
         
@@ -294,5 +322,265 @@ public class KanbanboardServiceTest {
 
         assertThat(findIssue.getContent()).isEqualTo(updateIssueRequest.getContent());
 
+    }
+
+    @Test
+    public void updateIssueChargerTest(){
+        Member member = new Member("bam", "wjs6265", "wb6265");
+
+        memberRepository.save(member);
+
+        Project project = new Project("프젝 1");
+
+        projectRepository.save(project);
+
+        ProjectMember projectMember = new ProjectMember(project, member);
+
+        projectMemberRepository.save(projectMember);
+
+        KanbanBoard kanbanBoard = new KanbanBoard("칸반보드", "KanbanBoard", project);
+        kanbanBoardRepository.save(kanbanBoard);
+
+        String kPath = "P" + project.getId() + "/K" + kanbanBoard.getId();
+        kanbanBoard.updatePath(kPath);
+
+        Issue issue = new Issue("이슈 테스트", "이슈 테스트 입니다.", 1, 1, kanbanBoard, null, null);
+
+        issueRepository.save(issue);
+
+        String iPath = kPath + "/I" + issue.getId();
+        issue.updatePath(iPath);
+
+        // 담당자 수정
+
+        kanbanBoardService.updateIssueMember(member.getId(), project.getId(), kanbanBoard.getId(), issue.getId(), member.getId());
+
+        em.flush();
+
+        Issue findIssue = issueRepository.findById(issue.getId()).orElseThrow(() -> new EntityNotFoundException());
+
+        assertThat(findIssue.getCharger()).isEqualTo(member);
+
+        // 라벨 수정
+
+        Label label = new Label("라벨 ", kanbanBoard);
+        labelRepository.save(label);
+
+        kanbanBoardService.updateIssueLabel(member.getId(), project.getId(), kanbanBoard.getId(), issue.getId(), label.getId());
+
+        em.flush();
+
+        Issue findIssueLabel = issueRepository.findById(issue.getId()).orElseThrow(() -> new EntityNotFoundException());
+
+        assertThat(findIssueLabel.getLabel()).isEqualTo(label);
+
+        // 중요도 수정
+
+        kanbanBoardService.updateIssueImportance(member.getId(), project.getId(), kanbanBoard.getId(), issue.getId(), 1);
+
+        em.flush();
+
+        Issue findIssueImportance = issueRepository.findById(issue.getId()).orElseThrow(() -> new EntityNotFoundException());
+
+        assertThat(findIssueImportance.getImportance()).isEqualTo(1);
+
+        // 상태 수정
+
+        kanbanBoardService.updateIssueStatus(member.getId(), project.getId(), kanbanBoard.getId(), issue.getId(), 3);
+
+        em.flush();
+
+        Issue findIssueStatus = issueRepository.findById(issue.getId()).orElseThrow(() -> new EntityNotFoundException());
+
+        assertThat(findIssueStatus.getStatus()).isEqualTo(3);
+    }
+
+    @Test
+    public void getIssues(){
+        // 멤버 생성
+        Member member = new Member("bam", "wjs6265", "wb6265");
+
+        memberRepository.save(member);
+        Member member2 = new Member("su", "su", "su");
+
+        memberRepository.save(member2);
+
+        Member[] members = {member, member2};
+
+        // 프로젝트 생성
+        Project project = new Project("프젝 1");
+
+        projectRepository.save(project);
+
+        // 프로젝트 멤버 생성
+        ProjectMember projectMember = new ProjectMember(project, member);
+
+        projectMemberRepository.save(projectMember);
+
+        // 칸반 보드 생성
+        KanbanBoard kanbanBoard = new KanbanBoard("칸반보드", "KanbanBoard", project);
+        kanbanBoardRepository.save(kanbanBoard);
+
+        String kPath = "P" + project.getId() + "/K" + kanbanBoard.getId();
+        kanbanBoard.updatePath(kPath);
+
+        // 칸반 보드 생성
+        KanbanBoard kanbanBoard2 = new KanbanBoard("칸반보드2", "KanbanBoard", project);
+        kanbanBoardRepository.save(kanbanBoard2);
+
+        String kPath2 = "P" + project.getId() + "/K" + kanbanBoard2.getId();
+        kanbanBoard.updatePath(kPath2);
+
+        // 이슈 생성
+        Issue issue = new Issue("이슈 테스트", "이슈 테스트 입니다.", 1, 1, kanbanBoard, null, null);
+
+        issueRepository.save(issue);
+
+        String iPath = kPath + "/I" + issue.getId();
+        issue.updatePath(iPath);
+
+        // 담당자 수정
+        kanbanBoardService.updateIssueMember(member.getId(), project.getId(), kanbanBoard.getId(), issue.getId(), member.getId());
+
+        // 라벨 수정
+        Label label = new Label("라벨", kanbanBoard);
+        labelRepository.save(label);
+
+        // 라벨 수정
+        Label label2 = new Label("라벨2", kanbanBoard);
+        labelRepository.save(label2);
+
+        Label[] labels = {label, label2};
+
+        kanbanBoardService.updateIssueLabel(member.getId(), project.getId(), kanbanBoard.getId(), issue.getId(), label.getId());
+
+        for (int i = 0; i < 100; i++) {
+            if(i % 2 == 0){
+                // 이슈 생성
+                Issue issues = new Issue("이슈 테스트" + i, "이슈 테스트 입니다.", (int)(Math.random() * 3) + 1, (int)(Math.random() * 3) + 1, kanbanBoard, null, null);
+
+                issueRepository.save(issues);
+
+                String iPath2 = kPath + "/I" + issue.getId();
+                issues.updatePath(iPath2);
+
+                int randomValue = (Math.random() < 0.5) ? 0 : 1;
+                issues.updateMember(members[randomValue]);
+
+                int randomValue2 = (Math.random() < 0.5) ? 0 : 1;
+                issues.updateLabel(labels[randomValue2]);
+            }else{
+                // 이슈 생성
+                Issue issues = new Issue("이슈 테스트" + i, "이슈 테스트 입니다.", (int)(Math.random() * 3) + 1, (int)(Math.random() * 3) + 1, kanbanBoard2, null, null);
+
+                issueRepository.save(issues);
+
+                String iPath2 = kPath + "/I" + issue.getId();
+                issues.updatePath(iPath2);
+            }
+
+        }
+
+        // 이슈 조회
+        GetIssueResponses getIssueResponses = kanbanBoardService.getIssues(member.getId(), project.getId(), kanbanBoard.getId(), member.getId(), label.getId(), 1);
+
+        for (int i = 0; i < getIssueResponses.issues().size(); i++) {
+            GetIssueResponse getIssueResponse = getIssueResponses.issues().get(i);
+
+            assertThat(getIssueResponse.charger().id()).isEqualTo(member.getId());
+            assertThat(getIssueResponse.label().labelId()).isEqualTo(label.getId());
+            assertThat(getIssueResponse.importance()).isEqualTo(1);
+        }
+    }
+
+    @Test
+    public void getIssueTest(){
+        // 멤버 생성
+        Member member = new Member("bam", "wjs6265", "wb6265");
+
+        memberRepository.save(member);
+        Member member2 = new Member("su", "su", "su");
+
+        memberRepository.save(member2);
+
+        Member[] members = {member, member2};
+
+        // 프로젝트 생성
+        Project project = new Project("프젝 1");
+
+        projectRepository.save(project);
+
+        // 프로젝트 멤버 생성
+        ProjectMember projectMember = new ProjectMember(project, member);
+
+        projectMemberRepository.save(projectMember);
+
+        // 칸반 보드 생성
+        KanbanBoard kanbanBoard = new KanbanBoard("칸반보드", "KanbanBoard", project);
+        kanbanBoardRepository.save(kanbanBoard);
+
+        String kPath = "P" + project.getId() + "/K" + kanbanBoard.getId();
+        kanbanBoard.updatePath(kPath);
+
+        // 칸반 보드 생성
+        KanbanBoard kanbanBoard2 = new KanbanBoard("칸반보드2", "KanbanBoard", project);
+        kanbanBoardRepository.save(kanbanBoard2);
+
+        String kPath2 = "P" + project.getId() + "/K" + kanbanBoard2.getId();
+        kanbanBoard.updatePath(kPath2);
+
+        // 이슈 생성
+        Issue issue = new Issue("이슈 테스트", "이슈 테스트 입니다.", 1, 1, kanbanBoard, null, null);
+
+        issueRepository.save(issue);
+
+        String iPath = kPath + "/I" + issue.getId();
+        issue.updatePath(iPath);
+
+        // 담당자 수정
+        kanbanBoardService.updateIssueMember(member.getId(), project.getId(), kanbanBoard.getId(), issue.getId(), member.getId());
+
+        // 라벨 수정
+        Label label = new Label("라벨", kanbanBoard);
+        labelRepository.save(label);
+
+        // 라벨 수정
+        Label label2 = new Label("라벨2", kanbanBoard);
+        labelRepository.save(label2);
+
+        Label[] labels = {label, label2};
+
+        kanbanBoardService.updateIssueLabel(member.getId(), project.getId(), kanbanBoard.getId(), issue.getId(), label.getId());
+
+        for (int i = 0; i < 100; i++) {
+            if(i % 2 == 0){
+                // 이슈 생성
+                Issue issues = new Issue("이슈 테스트" + i, "이슈 테스트 입니다.", (int)(Math.random() * 3) + 1, (int)(Math.random() * 3) + 1, kanbanBoard, null, null);
+
+                issueRepository.save(issues);
+
+                String iPath2 = kPath + "/I" + issue.getId();
+                issues.updatePath(iPath2);
+
+                int randomValue = (Math.random() < 0.5) ? 0 : 1;
+                issues.updateMember(members[randomValue]);
+
+                int randomValue2 = (Math.random() < 0.5) ? 0 : 1;
+                issues.updateLabel(labels[randomValue2]);
+            }else{
+                // 이슈 생성
+                Issue issues = new Issue("이슈 테스트" + i, "이슈 테스트 입니다.", (int)(Math.random() * 3) + 1, (int)(Math.random() * 3) + 1, kanbanBoard2, null, null);
+
+                issueRepository.save(issues);
+
+                String iPath2 = kPath + "/I" + issue.getId();
+                issues.updatePath(iPath2);
+            }
+
+        }
+
+        GetIssueDetailResponse getIssueDetailResponse = kanbanBoardService.getIssue(member.getId(), project.getId(), kanbanBoard.getId(), issue.getId());
+
+        assertThat(getIssueDetailResponse.issueId()).isEqualTo(issue.getId());
     }
 }
