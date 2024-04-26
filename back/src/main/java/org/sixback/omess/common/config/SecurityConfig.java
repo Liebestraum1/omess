f78e690aa6aa6082970cbece43a38778c7c915f9
bndgc@naver.com <bndgc@naver.com>
2024-04-26 14:25:38 +0900

    feat: SecurityConfig

---
+++ back/src/main/java/org/sixback/omess/common/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package org.sixback.omess.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.savedrequest.NullRequestCache;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .rememberMe(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .sessionManagement((configurer -> {
+                    configurer.sessionCreationPolicy((SessionCreationPolicy.NEVER));
+                    configurer.sessionFixation().migrateSession();
+                }))
+                .requestCache(configurer -> configurer.requestCache(new NullRequestCache()))
+                .build()
+                ;
+    }
+}
