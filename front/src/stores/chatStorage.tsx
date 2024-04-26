e9ac9e1f91629f393e89a8090fe76ee6e5693866
chlwodyd9829 <chlwodyd9829@naver.com>
2024-04-26 13:06:24 +0900

    chore: recoil -> zustand

---
+++ front/src/stores/chatStorage.tsx
@@ -0,0 +1,40 @@
+import {create} from "zustand";
+import {ChatMessage} from "../types/chat/chat.ts";
+
+type State = {
+    chatId: string | null,
+    messages: Array<ChatMessage> | null
+}
+
+type Action = {
+    setChatId: (value: string) => void;
+    setMessages: (value: Array<ChatMessage>) => void;
+    addMessage: (value: ChatMessage) => void;
+    getMessage: (messageId: string) => ChatMessage | null;
+    clear: () => void;
+}
+
+type ChatStore = State & Action;
+
+export const useChat = create<ChatStore>((set, get) => {
+    return {
+        chatId: null,
+        messages: null,
+        setChatId: (chatId: string) => {
+            set({chatId: chatId})
+        },
+        setMessages: (messages: Array<ChatMessage>) => {
+            set({messages: {...messages}})
+        },
+        addMessage: (message: ChatMessage) => {
+            set((state) => ({
+                messages: state.messages ? [...state.messages, message] : [message]
+            }))
+        },
+        getMessage: (messageId: string) => {
+            const messages = get().messages;
+            return messages ? messages.filter(message => message.messageId === messageId)[0] : null;
+        },
+        clear: () => set({chatId: null, messages: null})
+    }
+})
