c5195638ad49ab843c40f1a49b4fd9408c6a67a2
jsy6265 <71249591+jsy6265@users.noreply.github.com>
2024-04-26 14:30:09 +0900

    chore :  주석 제거

--- back/src/main/java/org/sixback/omess/domain/module/service/ModuleService.java
+++ back/src/main/java/org/sixback/omess/domain/module/service/ModuleService.java
@@ -9,13 +9,11 @@ import org.sixback.omess.domain.module.model.entity.Module;
 import org.sixback.omess.domain.module.model.entity.ModuleCategory;
 import org.sixback.omess.domain.module.repository.ModuleCategoryRepository;
 import org.sixback.omess.domain.module.repository.ModuleRepository;
-import org.sixback.omess.domain.project.model.entity.ProjectMember;
 import org.sixback.omess.domain.project.repository.ProjectMemberRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -40,11 +38,9 @@ public class ModuleService {
 
     @Transactional
     public void updateModule(Long memberId, Long moduleId, Long projectId, UpdateMouleRequest updateMouleRequest) {
-        // FixMe 요청자가 팀원인지 확인 로직 추가
         isProjectMember(projectId, memberId);
 
-        // FixMe 예외 처리
-        Module module = moduleRepository.findById(moduleId).orElseThrow(() -> new EntityNotFoundException());
+        Module module = moduleRepository.findById(moduleId).orElseThrow(() -> new EntityNotFoundException("잘못된 요청 입니다."));
 
         module.updateModule(updateMouleRequest.getTitle());
 
@@ -62,7 +58,6 @@ public class ModuleService {
         ).toList();
     }
 
-    // FixMe 에러 처리 수정
     // 프로젝트 멤버 유효성 검사
     private void isProjectMember(Long projectId, Long memberId) {
         projectMemberRepository.findByProjectIdAndMemberId(projectId, memberId).orElseThrow(() -> new EntityNotFoundException());
