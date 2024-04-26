58302a8da77e4b0fa258fc47cfe146e44cbeff61
doongdang <doongdang@naver.com>
2024-04-26 11:42:15 +0900

    test: API 명세서 생성 성공 테스트

--- back/src/test/java/org/sixback/omess/domain/apispecification/controller/ApiSpecificationControllerTest.java
+++ back/src/test/java/org/sixback/omess/domain/apispecification/controller/ApiSpecificationControllerTest.java
@@ -1,16 +1,16 @@
 package org.sixback.omess.domain.apispecification.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.sixback.omess.common.TestUtils;
+import org.sixback.omess.domain.apispecification.model.dto.CreateApiSpecificationRequest;
 import org.sixback.omess.domain.apispecification.model.entity.ApiSpecification;
 import org.sixback.omess.domain.apispecification.repository.ApiSpecificationRepository;
 import org.sixback.omess.domain.apispecification.repository.DomainRepository;
 import org.sixback.omess.domain.apispecification.service.ApiSpecificationService;
-import org.sixback.omess.domain.member.repository.MemberRepository;
+import org.sixback.omess.domain.apispecification.util.ApiSpecificationUtils;
 import org.sixback.omess.domain.project.model.entity.Project;
-import org.sixback.omess.domain.project.repository.ProjectMemberRepository;
 import org.sixback.omess.domain.project.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -18,6 +18,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -36,13 +44,7 @@ class ApiSpecificationControllerTest {
     ApiSpecificationRepository apiSpecificationRepository;
 
     @Autowired
-    MemberRepository memberRepository;
-
-    @Autowired
     ProjectRepository projectRepository;
-
-    @Autowired
-    ProjectMemberRepository projectMemberRepository;
 
     @Autowired
     DomainRepository domainRepository;
@@ -60,9 +62,7 @@ class ApiSpecificationControllerTest {
     @AfterEach
     public void cleanUpRepository() {
         apiSpecificationRepository.deleteAll();
-        projectMemberRepository.deleteAll();
         projectRepository.deleteAll();
-        memberRepository.deleteAll();
     }
 
     private ApiSpecification apiSpecification(Project project) {
@@ -71,6 +71,46 @@ class ApiSpecificationControllerTest {
                 .moduleCategory("testCategory")
                 .project(project)
                 .build();
+    }
+
+    @Test
+    @DisplayName("")
+    void newTest(){
+        //given
+        String contextPath = "/api/v1/projects/1/api-specifications";
+        String path = ApiSpecificationUtils.generatePath(contextPath, 1L);
+        //when
+
+        //then
+        assertThat(path).isEqualTo("P1/A1");
+    }
+
+    @Test
+    @DisplayName("API 명세서 생성 성공 테스트")
+    void apiSpecificationSuccessTest() throws Exception {
+        //given
+        CreateApiSpecificationRequest request = new CreateApiSpecificationRequest("testName", "testCategory");
+        Project project = TestUtils.makeProject();
+        projectRepository.save(project);
+
+        //when
+        mockMvc.perform(
+                    post("/api/v1/projects/{projectId}/api-specifications", project.getId())
+                            .contentType(APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request))
+                ).andExpect(status().isOk())
+                .andDo(print());
+
+        Optional<ApiSpecification> optionalApiSpecification = apiSpecificationRepository.findByProjectId(project.getId());
+
+        //then
+        assertThat(optionalApiSpecification.isPresent()).isTrue();
+
+        ApiSpecification apiSpecification = optionalApiSpecification.get();
+        assertThat(apiSpecification.getPath()).isEqualTo("P" + project.getId() + "/A" + apiSpecification.getId());
+        assertThat(apiSpecification.getTitle()).isEqualTo(request.name());
+        assertThat(apiSpecification.getCategory()).isEqualTo(request.category());
+
     }
 
 }
