bcc117027cf7c6ac2d2dfe5e328e3864fc9ca185
doongdang <doongdang@naver.com>
2024-04-26 10:49:14 +0900

    style: 오탈자 교정

--- back/src/test/resources/sql/schema-setup.sql
+++ back/src/test/resources/sql/schema-setup.sql
@@ -111,7 +111,7 @@ CREATE TABLE `api`
     FOREIGN KEY (domain_id) REFERENCES domain (id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXIST `http_method`
+CREATE TABLE IF NOT EXISTS `http_method`
 (
     `id`       BIGINT AUTO_INCREMENT PRIMARY KEY,
     `method`   VARCHAR(10) NOT NULL,
@@ -119,7 +119,7 @@ CREATE TABLE IF NOT EXIST `http_method`
     updated_at TIMESTAMP DEFAULT now() ON UPDATE now()
 );
 
-CREATE TABLE IF NOT EXIST `module_category`
+CREATE TABLE IF NOT EXISTS `module_category`
 (
     `id`       BIGINT AUTO_INCREMENT PRIMARY KEY,
     `category` VARCHAR(50) NOT NULL,
