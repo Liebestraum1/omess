3af8771c15170ca5a6a3cee0ca4a34b2581c39cd
jsy6265 <71249591+jsy6265@users.noreply.github.com>
2024-04-26 14:30:27 +0900

    feat : add UpdateIssueCommonRequest

---
+++ back/src/main/java/org/sixback/omess/domain/kanbanboard/model/dto/request/issue/UpdateIssueCommonRequest.java
@@ -0,0 +1,19 @@
+package org.sixback.omess.domain.kanbanboard.model.dto.request.issue;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class UpdateIssueCommonRequest {
+    Long chargerId;
+    Long labelId;
+    @Min(0) @Max(3)
+    Integer importance;
+    @Min(1) @Max(3)
+    Integer status;
+}
