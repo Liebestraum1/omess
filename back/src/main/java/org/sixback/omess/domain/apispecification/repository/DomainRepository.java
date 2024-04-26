62b914fa77bbaa966205c6f167fce745563611e6
doongdang <doongdang@naver.com>
2024-04-26 13:59:27 +0900

    feat: findByPath

--- back/src/main/java/org/sixback/omess/domain/apispecification/repository/DomainRepository.java
+++ back/src/main/java/org/sixback/omess/domain/apispecification/repository/DomainRepository.java
@@ -3,5 +3,8 @@ package org.sixback.omess.domain.apispecification.repository;
 import org.sixback.omess.domain.apispecification.model.entity.Domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DomainRepository extends JpaRepository<Domain, Long> {
+    Optional<Domain> findByPath(String path);
 }
