8232b317e0f2ba1bb6ded979dcd44be92fb7f839
doongdang <doongdang@naver.com>
2024-04-26 09:40:05 +0900

    fix: http_method 초기화 제거

--- back/src/test/resources/sql/schema-setup.sql
+++ back/src/test/resources/sql/schema-setup.sql
@@ -111,13 +111,13 @@ CREATE TABLE `api`
     FOREIGN KEY (domain_id) REFERENCES domain (id) ON DELETE CASCADE
 );
 
-CREATE TABLE `http_method`
-(
-    `id`       BIGINT AUTO_INCREMENT PRIMARY KEY,
-    `method`   VARCHAR(10) NOT NULL,
-    created_at TIMESTAMP DEFAULT now(),
-    updated_at TIMESTAMP DEFAULT now() ON UPDATE now()
-);
+-- CREATE TABLE `http_method`
+-- (
+--     `id`       BIGINT AUTO_INCREMENT PRIMARY KEY,
+--     `method`   VARCHAR(10) NOT NULL,
+--     created_at TIMESTAMP DEFAULT now(),
+--     updated_at TIMESTAMP DEFAULT now() ON UPDATE now()
+-- );
 
 CREATE TABLE `module_category`
 (
