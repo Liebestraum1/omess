6038804970fea57a3a37098e251a79b89bcab60e
doongdang <whtjrgus0428@gmail.com>
2024-04-28 21:21:13 +0900

    feat: constructor

--- back/src/main/java/org/sixback/omess/domain/apispecification/model/entity/RequestHeader.java
+++ back/src/main/java/org/sixback/omess/domain/apispecification/model/entity/RequestHeader.java
@@ -1,13 +1,19 @@
 package org.sixback.omess.domain.apispecification.model.entity;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import static jakarta.persistence.FetchType.*;
+import static jakarta.persistence.GenerationType.*;
+import static lombok.AccessLevel.*;
+
 import org.sixback.omess.common.BaseTimeEntity;
 
-import static jakarta.persistence.FetchType.LAZY;
-import static jakarta.persistence.GenerationType.IDENTITY;
-import static lombok.AccessLevel.PROTECTED;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
@@ -21,12 +27,18 @@ public class RequestHeader extends BaseTimeEntity {
     private String path;
 
     @Column(nullable = false, length = 50)
-    private String key;
+    private String headerKey;
 
     @Column(nullable = false, length = 100)
-    private String value;
+    private String headerValue;
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "api_id")
     Api api;
+
+    public RequestHeader(String headerKey, String headerValue, Api api) {
+        this.headerKey = headerKey;
+        this.headerValue = headerValue;
+        this.api = api;
+    }
 }
