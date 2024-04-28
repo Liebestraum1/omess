07ac8eb164188c8cad7d387e3c2a9f51602d2041
doongdang <whtjrgus0428@gmail.com>
2024-04-28 21:23:34 +0900

    fix: dto to entity에 부모 entity 추가

--- back/src/main/java/org/sixback/omess/domain/apispecification/util/ApiSpecificationMapper.java
+++ back/src/main/java/org/sixback/omess/domain/apispecification/util/ApiSpecificationMapper.java
@@ -3,20 +3,21 @@ package org.sixback.omess.domain.apispecification.util;
 import org.sixback.omess.domain.apispecification.model.dto.CreatePathVariableRequest;
 import org.sixback.omess.domain.apispecification.model.dto.CreateQueryParamRequest;
 import org.sixback.omess.domain.apispecification.model.dto.CreateRequestHeaderRequest;
+import org.sixback.omess.domain.apispecification.model.entity.Api;
 import org.sixback.omess.domain.apispecification.model.entity.PathVariable;
 import org.sixback.omess.domain.apispecification.model.entity.QueryParam;
 import org.sixback.omess.domain.apispecification.model.entity.RequestHeader;
 
 public class ApiSpecificationMapper {
-	public static RequestHeader toRequestHeader(CreateRequestHeaderRequest request){
-		return new RequestHeader(request.key(), request.value());
+	public static RequestHeader toRequestHeader(CreateRequestHeaderRequest request, Api api){
+		return new RequestHeader(request.headerKey(), request.headerValue(), api);
 	}
 
-	public static QueryParam toQueryParam(CreateQueryParamRequest request){
-		return new QueryParam(request.name(), request.description());
+	public static QueryParam toQueryParam(CreateQueryParamRequest request, Api api){
+		return new QueryParam(request.name(), request.description(), api);
 	}
 
-	public static PathVariable toPathVariable(CreatePathVariableRequest request){
-		return new PathVariable(request.name(), request.description());
+	public static PathVariable toPathVariable(CreatePathVariableRequest request, Api api){
+		return new PathVariable(request.name(), request.description(), api);
 	}
 }
