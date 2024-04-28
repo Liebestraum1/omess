6200fbb128c501e88fea6350d963b83921ceedac
박상현 <psnew14@gmail.com>
2024-04-29 01:20:55 +0900

    feat: login form type check

--- front/src/components/Login/LoginButton.tsx
+++ front/src/components/Login/LoginButton.tsx
@@ -1,35 +1,17 @@
 import { Button } from "@mui/material";
-import { useLoginStore } from "../../stores/LoginStorage";
-import { useNavigate } from "react-router-dom";
 import { LoginStatusProps } from "../../types/Login/LoginProps";
 import { useCallback, useEffect, useState } from "react";
 
 const LoginButton = ({ loginStatus }: { loginStatus: LoginStatusProps }) => {
-    const navigate = useNavigate();
-    const { setServerLogin, setUserLogin } = useLoginStore();
-
     const [buttonText, setButtonText] = useState<string>("");
-    const [buttonFunction, setButtonFunction] = useState<() => void>(() => undefined);
 
-    const updateButtonState = useCallback(
-        (loginStatus: LoginStatusProps) => {
-            if (loginStatus === "none") {
-                setButtonText("서버 접속");
-                setButtonFunction(() => setServerLogin);
-            } else if (loginStatus === "server") {
-                setButtonText("로그인");
-                setButtonFunction(() => () => {
-                    setUserLogin();
-                    navigate("/main");
-                });
-            } else {
-                setButtonFunction(() => () => {
-                    navigate("/main");
-                });
-            }
-        },
-        [navigate, setServerLogin, setUserLogin]
-    );
+    const updateButtonState = useCallback((loginStatus: LoginStatusProps) => {
+        if (loginStatus === "none") {
+            setButtonText("서버 접속");
+        } else {
+            setButtonText("로그인");
+        }
+    }, []);
 
     useEffect(() => {
         updateButtonState(loginStatus);
@@ -42,7 +24,7 @@ const LoginButton = ({ loginStatus }: { loginStatus: LoginStatusProps }) => {
                 fontSize: 16,
             }}
             variant="contained"
-            onClick={buttonFunction}
+            type="submit"
         >
             {buttonText}
         </Button>
--- front/src/components/Login/LoginForm.tsx
+++ front/src/components/Login/LoginForm.tsx
@@ -3,6 +3,8 @@ import LoginButton from "./LoginButton";
 import LoginFormInput from "./LoginFormInput";
 import { LoginStatusProps, LoginFormInputProp } from "../../types/Login/LoginProps";
 import { useEffect, useState } from "react";
+import { useLoginStore } from "../../stores/LoginStorage";
+import { useNavigate } from "react-router";
 
 const LoginFormBox = styled(Box)({
     display: "flex",
@@ -11,26 +13,67 @@ const LoginFormBox = styled(Box)({
 });
 
 const LoginForm = ({ loginStatus }: { loginStatus: LoginStatusProps }) => {
+    const navigator = useNavigate();
+    const { setServerLogin, setUserLogin } = useLoginStore();
+
     const [loginFormInputProps, setLoginFormInputProps] = useState<LoginFormInputProp[]>([]);
+
+    const [serverUrl, setServerUrl] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [loginFormSubmitFuction, setLoginFormSubmitFuction] = useState<() => void>(() => {});
+
+    const serverLoginFormHandler = () => {
+        const loginFormData = new FormData();
+        loginFormData.append("url", serverUrl);
+        setServerLogin();
+    };
+
+    const userLoginFormHandler = () => {
+        const loginFormData = new FormData();
+        loginFormData.append("email", email);
+        loginFormData.append("password", password);
+        setUserLogin();
+        navigator("/main");
+    };
 
     useEffect(() => {
         if (loginStatus == "none") {
-            setLoginFormInputProps([{ label: "SERVER URL / PORT", helperText: "서버의 주소와 포트를 입력하세요." }]);
+            setLoginFormInputProps([
+                {
+                    type: "text",
+                    label: "SERVER URL / PORT",
+                    helperText: "서버의 주소와 포트를 입력하세요.",
+                    onFormData: setServerUrl,
+                },
+            ]);
+
+            setLoginFormSubmitFuction(() => () => serverLoginFormHandler());
         } else {
             setLoginFormInputProps([
-                { label: "EMAIL", helperText: "이메일을 입력하세요." },
-                { label: "PASSWORLD", helperText: "비밀번호를 입력하세요." },
+                { type: "email", label: "EMAIL", helperText: "이메일을 입력하세요.", onFormData: setEmail },
+                { type: "password", label: "PASSWORD", helperText: "비밀번호를 입력하세요.", onFormData: setPassword },
             ]);
+
+            setLoginFormSubmitFuction(() => () => userLoginFormHandler());
         }
     }, [loginStatus]);
 
     return (
-        <LoginFormBox>
-            {loginFormInputProps.map((loginFormInputProp, index) => (
+        <LoginFormBox
+            component={"form"}
+            onSubmit={(e) => {
+                e.preventDefault();
+                loginFormSubmitFuction();
+            }}
+        >
+            {loginFormInputProps.map((loginFormInputProp) => (
                 <LoginFormInput
-                    key={index}
+                    key={loginFormInputProp.label}
+                    type={loginFormInputProp.type}
                     label={loginFormInputProp.label}
                     helperText={loginFormInputProp.helperText}
+                    onFormData={loginFormInputProp.onFormData}
                 ></LoginFormInput>
             ))}
             <LoginButton loginStatus={loginStatus}></LoginButton>
