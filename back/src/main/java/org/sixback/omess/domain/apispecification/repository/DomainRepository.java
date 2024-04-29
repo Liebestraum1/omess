7a84d33d1d886ec34265f83b0b39caff9072e5bb
doongdang <doongdang@naver.com>
2024-04-29 11:53:16 +0900

    feat: 도메인 삭제

--- back/src/main/java/org/sixback/omess/domain/apispecification/repository/DomainRepository.java
+++ back/src/main/java/org/sixback/omess/domain/apispecification/repository/DomainRepository.java
@@ -1,10 +1,14 @@
 package org.sixback.omess.domain.apispecification.repository;
 
+import java.util.Optional;
+
 import org.sixback.omess.domain.apispecification.model.entity.Domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface DomainRepository extends JpaRepository<Domain, Long> {
     Optional<Domain> findByPath(String path);
+
+	void deleteByPath(String path);
+
+	boolean existsByPath(String path);
 }
