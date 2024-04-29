25b5b0c4b1330b227c3f979cb4e3168ba3a3d114
doongdang <doongdang@naver.com>
2024-04-29 09:35:38 +0900

    fix: GsonParser 재사용화

--- back/src/main/java/org/sixback/omess/domain/apispecification/util/ApiSpecificationUtils.java
+++ back/src/main/java/org/sixback/omess/domain/apispecification/util/ApiSpecificationUtils.java
@@ -6,64 +6,66 @@ import org.sixback.omess.domain.apispecification.exception.InvalidApiInputExcept
 import org.springframework.boot.json.GsonJsonParser;
 
 public class ApiSpecificationUtils {
-    private static String removePrefix(String uri){
-        return uri.replace("/api/v1/", "");
-    }
+    private static final GsonJsonParser parser = new GsonJsonParser();
 
-    public static String generatePath(String uri, Long lastId){
-        StringBuilder path = new StringBuilder();
-        uri = removePrefix(uri);
-        String[] split = uri.split("/");
+	private static String removePrefix(String uri) {
+		return uri.replace("/api/v1/", "");
+	}
 
-        for (int i = 0; i < split.length; i++) {
-            if(i % 2 == 0){
-                path.append(Character.toUpperCase(split[i].charAt(0)));
-            }else{
-                path.append(split[i]);
-                path.append("/");
-            }
-        }
-        path.append(lastId);
+	public static String generatePath(String uri, Long lastId) {
+		StringBuilder path = new StringBuilder();
+		uri = removePrefix(uri);
+		String[] split = uri.split("/");
 
-        return path.toString();
-    }
+		for (int i = 0; i < split.length; i++) {
+			if (i % 2 == 0) {
+				path.append(Character.toUpperCase(split[i].charAt(0)));
+			} else {
+				path.append(split[i]);
+				path.append("/");
+			}
+		}
+		path.append(lastId);
 
-    public static String generateEstimatedCurrentPath(String uri){
-        StringBuilder path = new StringBuilder();
-        uri = removePrefix(uri);
-        String[] split = uri.split("/");
+		return path.toString();
+	}
 
-        for (int i = 0; i < split.length; i++) {
-            if(i % 2 == 0){
-                path.append(Character.toUpperCase(split[i].charAt(0)));
-            }else{
-                path.append(split[i]);
-                if (i < split.length - 1){
-                    path.append("/");
-                }
-            }
-        }
+	public static String generateEstimatedCurrentPath(String uri) {
+		StringBuilder path = new StringBuilder();
+		uri = removePrefix(uri);
+		String[] split = uri.split("/");
 
-        return path.toString();
-    }
+		for (int i = 0; i < split.length; i++) {
+			if (i % 2 == 0) {
+				path.append(Character.toUpperCase(split[i].charAt(0)));
+			} else {
+				path.append(split[i]);
+				if (i < split.length - 1) {
+					path.append("/");
+				}
+			}
+		}
 
-    public static String generateEstimatedParentPath(String uri, Long parentId){
-        uri = uri.substring(0, uri.lastIndexOf('/'));
-        String parentUri = uri.substring(0, uri.lastIndexOf('/'));
+		return path.toString();
+	}
 
-        return generatePath(parentUri, parentId);
-    }
+	public static String generateEstimatedParentPath(String uri, Long parentId) {
+		uri = uri.substring(0, uri.lastIndexOf('/'));
+		String parentUri = uri.substring(0, uri.lastIndexOf('/'));
 
-    public static void checkIsValidJsonSchema(String schemaString){
-        if(schemaString == null){
-            return;
-        }
-        GsonJsonParser parser = new GsonJsonParser();
-        //FIXME 추후 JSON Schema 자체에 대한 유효성 검사 방법을 찾아서 추가
-        try {
-            parser.parseMap(schemaString);
-        } catch (Exception e) {
-            throw new InvalidApiInputException(INVALID_JSON.getMessage());
-        }
-    }
+		return generatePath(parentUri, parentId);
+	}
+
+	public static void checkIsValidJsonSchema(String schemaString) {
+		if (schemaString == null) {
+			return;
+		}
+
+		//FIXME 추후 JSON Schema 자체에 대한 유효성 검사 방법을 찾아서 추가
+		try {
+			parser.parseMap(schemaString);
+		} catch (Exception e) {
+			throw new InvalidApiInputException(INVALID_JSON.getMessage());
+		}
+	}
 }
