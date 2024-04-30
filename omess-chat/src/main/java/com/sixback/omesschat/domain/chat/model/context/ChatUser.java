fbf45e18a8e8ed8062f6a7d9517687952cbc670a
chlwodyd9829 <chlwodyd9829@naver.com>
2024-04-30 17:31:38 +0900

    add: 채팅 관리 context

---
+++ omess-chat/src/main/java/com/sixback/omesschat/domain/chat/model/context/ChatUser.java
@@ -0,0 +1,53 @@
+package com.sixback.omesschat.domain.chat.model.context;
+
+import lombok.Getter;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+@Getter
+public class ChatUser {
+    private Long memberId;
+
+    private WebSocketSession session;
+
+    private ChatUser() {}
+    private ChatUser(WebSocketSession session) {
+        this.session = session;
+    }
+
+    private ChatUser(WebSocketSession session, Long memberId) {
+        this.session = session;
+        this.memberId = memberId;
+    }
+
+    public static ChatUser of(WebSocketSession session) {
+        return new ChatUser(session);
+    }
+
+    public static ChatUser of(WebSocketSession session, Long memberId) {
+        return new ChatUser(session, memberId);
+    }
+
+    public Mono<Void> update(String message) {
+        Mono<WebSocketMessage> webSocketMessage = Mono.just(session.textMessage(message));
+        return session.send(webSocketMessage)
+                .doOnSuccess(unused -> System.out.println("메시지 보내기 성공"));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChatUser chatUser = (ChatUser) o;
+        return Objects.equals(session, chatUser.session);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(session);
+    }
+}
+
