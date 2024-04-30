4de39d28c052466dbe21372fa7b0c7578ab38e5b
chlwodyd9829 <chlwodyd9829@naver.com>
2024-04-30 17:36:29 +0900

    feat: 웹소켓 채팅 연결, 저장, 종료 기능

--- omess-chat/src/main/java/com/sixback/omesschat/common/handler/ReactiveWebSocketHandler.java
+++ omess-chat/src/main/java/com/sixback/omesschat/common/handler/ReactiveWebSocketHandler.java
@@ -1,5 +1,13 @@
 package com.sixback.omesschat.common.handler;
 
+import com.sixback.omesschat.domain.chat.model.dto.ChatMessageDto;
+import com.sixback.omesschat.domain.chat.model.message.EnterMessage;
+import com.sixback.omesschat.domain.chat.model.message.Message;
+import com.sixback.omesschat.domain.chat.model.message.MessageType;
+import com.sixback.omesschat.domain.chat.model.message.SendMessage;
+import com.sixback.omesschat.domain.chat.parser.MessageParser;
+import com.sixback.omesschat.domain.chat.service.ChatService;
+import com.sixback.omesschat.domain.chat.service.WebSocketSessionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -14,25 +22,54 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class ReactiveWebSocketHandler implements WebSocketHandler {
 
+    private final WebSocketSessionService sessionService;
+    private final ChatService chatService;
+
+    /**
+     * 웹 소켓의 데이터 처리
+     */
     @Override
     public Mono<Void> handle(WebSocketSession session) {
         Flux<WebSocketMessage> receive = session.receive();
-
-        log.info("여기까지 안오나????");
-        receive.subscribe(
-                message -> {
-                    log.info("receive message {}", message.getPayload());
-                },
-                error -> {
-                    log.error("ERROR : {}", error.getMessage());
+        return receive.doOnSubscribe(message -> log.info("WebSocket 연결"))
+                .map(message -> MessageParser.parseMessage(message.getPayloadAsText()))
+                .flatMap(message -> process(session, message))
+                .flatMap(o -> after(session, o))
+                .doOnError((error) -> {
                     error.printStackTrace();
-                },
-                () -> {
-                    log.info("WebSocket Connect !!!");
-                }
-        );
-
-        return Mono.empty();
+                    log.error("에러 발생 : {} - {}", error.getCause(), error.getMessage());
+                })
+                .doFinally(type -> sessionService.leave(session))
+                .then();
     }
 
+    /**
+     * 메시지 처리
+     */
+    private Mono<?> process(WebSocketSession session, Message message) {
+        MessageType type = message.getType();
+        return switch (type) {
+            case ENTER -> sessionService
+                    .enter(session, MessageParser.parseMessage(message.getData(), EnterMessage.class));
+            case SEND -> {
+                SendMessage sendMessage = MessageParser.parseMessage(message.getData(), SendMessage.class);
+
+                String chatId = sessionService.findChatId(session);
+                Long memberId = sessionService.findMemberId(session);
+
+                yield chatService.saveChatMessage(chatId, memberId, sendMessage);
+            }
+            default -> throw new TypeNotPresentException(type.name(), new Throwable("Not Present MessageType"));
+        };
+    }
+
+    /**
+     * 처리가 완료된 데이터 사용자에게 송신
+     */
+    private Mono<Void> after(WebSocketSession session, Object data) {
+        if (data instanceof ChatMessageDto) {
+            return sessionService.send(session, (ChatMessageDto) data);
+        }
+        throw new ClassCastException();
+    }
 }
