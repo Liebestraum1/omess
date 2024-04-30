feae0a4b7c2cae940a23601a7a941d9e5f4de335
chlwodyd9829 <chlwodyd9829@naver.com>
2024-04-30 17:34:54 +0900

    add: 웹소켓 채팅 payload

---
+++ omess-chat/src/main/java/com/sixback/omesschat/domain/chat/model/message/EnterMessage.java
@@ -0,0 +1,9 @@
+package com.sixback.omesschat.domain.chat.model.message;
+
+import lombok.Getter;
+
+@Getter
+public class EnterMessage {
+    private String chatId;
+    private Long memberId;
+}
---
+++ omess-chat/src/main/java/com/sixback/omesschat/domain/chat/model/message/SendMessage.java
@@ -0,0 +1,8 @@
+package com.sixback.omesschat.domain.chat.model.message;
+
+import lombok.Getter;
+
+@Getter
+public class SendMessage {
+    private String message;
+}
