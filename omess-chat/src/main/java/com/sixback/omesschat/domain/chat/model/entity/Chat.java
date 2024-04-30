7a1dfc1bf74d6cef078ee2e6dd2daec9fe0daf36
chlwodyd9829 <chlwodyd9829@naver.com>
2024-04-30 17:29:56 +0900

    add: Chat, ChatMessage Entity

---
+++ omess-chat/src/main/java/com/sixback/omesschat/domain/chat/model/entity/Chat.java
@@ -0,0 +1,26 @@
+package com.sixback.omesschat.domain.chat.model.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.util.List;
+
+@Document
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Chat {
+
+    @MongoId
+    private String id;
+
+    private Long projectId;
+    private List<ChatMember> members;
+
+    public Chat(Long projectId, List<ChatMember> members) {
+        this.projectId = projectId;
+        this.members = members;
+    }
+}
