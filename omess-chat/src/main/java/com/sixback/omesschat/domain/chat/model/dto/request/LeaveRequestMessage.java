108951ab9ea0ba8edd2a4a3be3f902235ea83990
j-yong98 <chlwodyd9829@gmail.com>
2024-05-01 22:46:52 +0900

    move: Message > dto/request

---
+++ omess-chat/src/main/java/com/sixback/omesschat/domain/chat/model/dto/request/LeaveRequestMessage.java
@@ -0,0 +1,9 @@
+package com.sixback.omesschat.domain.chat.model.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class LeaveRequestMessage {
+
+    private String chatId;
+}
--- omess-chat/src/main/java/com/sixback/omesschat/domain/chat/model/message/EnterMessage.java
+++
@@ -1,9 +0,0 @@
-package com.sixback.omesschat.domain.chat.model.message;
-
-import lombok.Getter;
-
-@Getter
-public class EnterMessage {
-    private String chatId;
-    private Long memberId;
-}
