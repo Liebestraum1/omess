038f902a8681c970672ddc191fb584364689c476
j-yong98 <chlwodyd9829@gmail.com>
2024-05-01 22:53:34 +0900

    chore: return 타입 변경

--- omess-chat/src/main/java/com/sixback/omesschat/domain/chat/service/ChatService.java
+++ omess-chat/src/main/java/com/sixback/omesschat/domain/chat/service/ChatService.java
@@ -1,16 +1,19 @@
 package com.sixback.omesschat.domain.chat.service;
 
+import static com.sixback.omesschat.domain.chat.model.dto.response.ResponseType.HISTORY;
+import static com.sixback.omesschat.domain.chat.model.dto.response.ResponseType.MESSAGE;
+
 import com.sixback.omesschat.domain.chat.mapper.ChatMessageMapper;
-import com.sixback.omesschat.domain.chat.model.dto.ChatMessageDto;
+import com.sixback.omesschat.domain.chat.model.dto.request.SendRequestMessage;
+import com.sixback.omesschat.domain.chat.model.dto.response.ResponseMessage;
 import com.sixback.omesschat.domain.chat.model.entity.ChatMessage;
-import com.sixback.omesschat.domain.chat.model.message.SendMessage;
 import com.sixback.omesschat.domain.chat.repository.ChatMessageRepository;
 import com.sixback.omesschat.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 @Slf4j
 @Service
@@ -21,7 +24,10 @@ public class ChatService {
     private final MemberService memberService;
     private final ChatMessageRepository chatMessageRepository;
 
-    public Mono<ChatMessageDto> saveChatMessage(String chatId, Long memberId, SendMessage message) {
+    /**
+     * 채팅 메시지를 DB에 저장하는 기능
+     */
+    public Flux<ResponseMessage> saveChatMessage(String chatId, Long memberId, SendRequestMessage message) {
         ChatMessage chatMessage = ChatMessageMapper.to(chatId, memberId, message.getMessage());
         return chatMessageRepository.save(chatMessage)
                 .flatMap(c -> memberService
@@ -29,6 +35,8 @@ public class ChatService {
                         .map(member -> ChatMessageMapper.toResponse(c, member))
                 )
                 .doOnSuccess(chatMessageDto -> log.info("저장 성공 !!!"))
-                .doOnError(throwable -> log.error("에러 발생 - {} : {}", throwable.getCause(), throwable.getMessage()));
+                .doOnError(throwable -> log.error("에러 발생 - {} : {}", throwable.getCause(), throwable.getMessage()))
+                .map(chatMessageDto -> ResponseMessage.ok(MESSAGE, chatMessageDto))
+                .flux();
     }
 }
