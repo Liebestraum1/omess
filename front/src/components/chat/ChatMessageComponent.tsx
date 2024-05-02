0dfa8093d53f8be6b240c773fddc6882ccafecb3
chlwodyd9829 <chlwodyd9829@naver.com>
2024-05-02 17:52:20 +0900

    chore: chat history ui

--- front/src/components/chat/ChatMessageComponent.tsx
+++ front/src/components/chat/ChatMessageComponent.tsx
@@ -1,21 +1,19 @@
-import {Box, Button} from "@mui/material";
+import {Box} from "@mui/material";
 import {ChatMessage} from "../../types/chat/chat.ts";
 import {useEffect, useRef, useState} from "react";
+import ChatMessageMenu from "./ChatMessageMenu.tsx";
+import ChatMessageEditing from "./ChatMessageEditing.tsx";
+import TextField from "@mui/material/TextField";
 import {useChatStorage} from "../../stores/chatStorage.tsx";
 
 const ChatMessageComponent = (message: ChatMessage) => {
     const {sendMessage} = useChatStorage();
-    const [isEditing, setEditing] = useState(false);
-    const inputRef = useRef<HTMLInputElement>(null);
-    const [inputValue, setInputValue] = useState<string>('')
+    const [isEditing, setIsEditing] = useState(false);
     const [date, setDate] = useState<string>('');
     const [time, setTime] = useState<string>('');
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [inputValue, setInputValue] = useState<string>(message.message)
 
-    useEffect(() => {
-        if (isEditing) {
-            inputRef.current?.focus();
-        }
-    }, [isEditing]);
 
     function transTime(t: string[]) {
         const str = (parseInt(t[0]) / 12) < 1 ? '오전 ' : '오후 ';
@@ -41,23 +39,19 @@ const ChatMessageComponent = (message: ChatMessage) => {
         setTime(res);
     }, [message]);
 
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleInputChange = (e: any) => {
         setInputValue(e.target.value);
     };
 
-    const setUpdate = () => {
-        setEditing(true);
-        setInputValue(message.message);
-    }
-
-    const update = (messageId: string, before: string) => {
-        const message = inputValue.trim()
-        if (message!.length === 0 || message!.length >= 16383) {
+    const update = () => {
+        const before = message.message;
+        const m = inputValue.trim()
+        if (m!.length === 0 || m!.length >= 16383) {
             console.error('길이 제한')
             return;
         }
 
-        if (before === message) {
+        if (before === m) {
             console.error('변경 내용 없음')
             return;
         }
@@ -65,62 +59,58 @@ const ChatMessageComponent = (message: ChatMessage) => {
         const data = {
             type: 'UPDATE',
             data: {
-                messageId: messageId,
-                message: message
+                messageId: message.id,
+                message: m
             }
         }
 
         sendMessage(JSON.stringify(data))
-        setEditing(false);
+        setIsEditing(false);
     }
 
-    const deleteMessage = (id: string) => {
+    const deleteMessage = () => {
         const data = {
             type: 'DELETE',
             data: {
-                messageId: id,
+                messageId: message.id,
             }
         }
 
         sendMessage(JSON.stringify(data))
     }
+
+
     return (
         <Box
             alignItems="center"
             display="flex"
+            justifyContent="space-between"
             gap={3}
             padding={2}
         >
             {/*<Avatar*/}
             {/*    src={value.member.}*/}
             {/*/>*/}
-            <Box>
-                <Box display="flex" gap={2}>
-                    <Box sx={{fontWeight: 'bold'}}>{message.member.nickname}</Box>
-                    <span>{date + ' ' + time}</span>
-                    <span>{message.isUpdated ? '(수정됨)' : ''}</span>
-                </Box>
-                <Box>
-                    {isEditing ? <input
-                            ref={inputRef}
-                            type="text"
-                            value={inputValue}
-                            onChange={handleInputChange}
-                        /> :
-                        <span> {message.message}</span>
-                    }
-                </Box>
-            </Box>
+            {
+                !isEditing ?
+                    <Box>
+                        <Box display="flex" gap={2}>
+                            <Box sx={{fontWeight: 'bold'}}>{message.member.nickname}</Box>
+                            <span>{date + ' ' + time}</span>
+                            <span>{message.isUpdated ? '(수정됨)' : ''}</span>
+                        </Box>
+                        <Box>{message.message}</Box>
+                    </Box> :
+                    <TextField
+                        ref={inputRef}
+                        value={inputValue}
+                        onChange={(e) => handleInputChange(e)}
+                        label="메시지" sx={{width: '40%'}}/>
+            }
             {!isEditing ?
-                <Box>
-                    <Button onClick={() => setUpdate()}>메시지 수정</Button>
-                    <Button onClick={() => deleteMessage(message.id)}>메시지 삭제</Button>
-                </Box>
+                <ChatMessageMenu setIsEditing={setIsEditing} delete={deleteMessage}/>
                 :
-                <Box>
-                    <Button onClick={() => update(message.id, message.message)}>수정</Button>
-                    <Button onClick={() => setEditing(false)}>취소</Button>
-                </Box>
+                <ChatMessageEditing update={update} setIsEditing={setIsEditing}/>
             }
         </Box>
     );
