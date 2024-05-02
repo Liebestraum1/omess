3b4bf6da33316a74755dfd442ac04438b4171504
chlwodyd9829 <chlwodyd9829@naver.com>
2024-05-02 16:16:04 +0900

    add: chatStorage

--- front/src/stores/chatStorage.tsx
+++ front/src/stores/chatStorage.tsx
@@ -1,40 +1,125 @@
-import { create } from "zustand";
-import { ChatMessage } from "../types/chat/chat.ts";
+import {create} from "zustand";
+import {ChatInfo, ChatMessage, Writer} from "../types/chat/chat.ts";
 
-type State = {
+type ChatStorage = {
     chatId: string | null;
+    serverUrl: string;
+    client: WebSocket | null;
+    members: Writer[] | null;
     messages: Array<ChatMessage> | null;
+    init: (chat: ChatInfo) => void;
+    sendMessage: (data: any) => void;
 };
 
-type Action = {
-    setChatId: (value: string) => void;
-    setMessages: (value: Array<ChatMessage>) => void;
-    addMessage: (value: ChatMessage) => void;
-    getMessage: (messageId: string) => ChatMessage | null;
-    clear: () => void;
-};
+export const useChatStorage = create<ChatStorage>((set, get) => {
+    const addLast = (message: ChatMessage) => {
+        const messages = get().messages;
+        if (messages == null) {
+            set({messages: [message]})
+        } else {
+            messages.push(message);
+            set({messages: messages})
+        }
+    }
+    const addFirst = (message: ChatMessage) => {
+        const messages = get().messages;
+        if (messages == null) {
+            set({messages: [message]})
+        } else {
+            messages.unshift(message);
+            set({messages: messages})
+        }
+    }
 
-type ChatStore = State & Action;
+    const updateMessage = (message: ChatMessage) => {
+        const messages = get().messages;
 
-export const useChat = create<ChatStore>((set, get) => {
-    return {
-        chatId: null,
-        messages: null,
-        setChatId: (chatId: string) => {
-            set({ chatId: chatId });
-        },
-        setMessages: (messages: Array<ChatMessage>) => {
-            set({ messages: { ...messages } });
-        },
-        addMessage: (message: ChatMessage) => {
-            set((state) => ({
-                messages: state.messages ? [...state.messages, message] : [message],
-            }));
-        },
-        getMessage: (messageId: string) => {
-            const messages = get().messages;
-            return messages ? messages.filter((message) => message.messageId === messageId)[0] : null;
-        },
-        clear: () => set({ chatId: null, messages: null }),
+        const updatedMessages = messages!.map(msg =>
+            msg.id === message.id ? {...msg, message: message.message, isUpdated: message.isUpdated} : msg
+        );
+
+        set({messages: updatedMessages});
+    }
+
+    const deleteMessage = (message: ChatMessage) => {
+        const messages = get().messages;
+
+        const deleteMessages = messages!.filter(msg =>
+            msg.id !== message.id
+        )
+
+        set({messages: deleteMessages});
+    }
+
+    const addMember = (writer: Writer) => {
+        const members = get().members;
+        if (members == null) {
+            set({members: [writer]})
+        } else {
+            get().members!.push(writer)
+            set({members: members})
+        }
     };
+
+    return {
+        serverUrl: 'ws://localhost:8081/chat/v1',
+        chatId: null,
+        client: null,
+        members: null,
+        messages: null,
+        init: (chat: ChatInfo) => {
+            if (chat == null) {
+                console.log('must have chatInfo')
+                return;
+            }
+
+            const url = get().serverUrl
+            const client = new WebSocket(url)
+            set({client, chatId: chat.id})
+
+            client.onopen = () => {
+                const enter = {
+                    type: 'ENTER',
+                    data: {
+                        memberId: 1,
+                        chatId: `${get().chatId}`
+                    }
+                };
+                client.send(JSON.stringify(enter));
+            }
+
+            client.onerror = (error) => {
+                console.error("에러발생!!!", error);
+            };
+
+            client.onmessage = (event) => {
+                const message = JSON.parse(event.data);
+                console.log(message.type, message.data)
+                switch (message.type) {
+                    case "MESSAGE":
+                        addFirst(message.data);
+                        break;
+                    case "HISTORY":
+                        addLast(message.data);
+                        break;
+                    case "UPDATE":
+                        updateMessage(message.data);
+                        break;
+                    case "DELETE":
+                        deleteMessage(message.data);
+                        break;
+                    case "SUCCESS":
+                        console.log("연결 성공")
+                        break;
+                }
+            };
+        },
+        sendMessage: (data: any) => {
+            if (get().client == null) {
+                console.error('client 연결 안돼 있음')
+            } else {
+                get().client!.send(data)
+            }
+        }
+    }
 });
