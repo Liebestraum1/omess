caf9d70748f47eef730ed037b1209106158e2850
chlwodyd9829 <chlwodyd9829@naver.com>
2024-05-02 16:17:44 +0900

    add: chatting header component

---
+++ front/src/components/chat/ChatHeaderDataComponent.tsx
@@ -0,0 +1,11 @@
+import {ChatMessage} from "../../types/chat/chat.ts";
+
+const ChatHeaderDataComponent = ({header}: {header: ChatMessage | undefined}) => {
+    return (
+        <>
+            <span>{header?.message}</span>
+        </>
+    );
+}
+
+export default ChatHeaderDataComponent;
