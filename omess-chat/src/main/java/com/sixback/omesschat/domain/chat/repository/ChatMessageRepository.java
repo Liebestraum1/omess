6a1b9b50d3216cc8c9c5bbb67cde8ec9afe2c456
chlwodyd9829 <chlwodyd9829@naver.com>
2024-05-02 12:44:02 +0900

    chore: 삭제 처리된 메시지 불러오지 않기

--- omess-chat/src/main/java/com/sixback/omesschat/domain/chat/repository/ChatMessageRepository.java
+++ omess-chat/src/main/java/com/sixback/omesschat/domain/chat/repository/ChatMessageRepository.java
@@ -1,10 +1,12 @@
 package com.sixback.omesschat.domain.chat.repository;
 
 import com.sixback.omesschat.domain.chat.model.entity.ChatMessage;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
 
 public interface ChatMessageRepository extends ReactiveMongoRepository<ChatMessage, String> {
 
+    @Query("{chatId: ?0, writer: ?1, isDeleted:  false}")
     Flux<ChatMessage> findAllByChatIdAndWriter(String chatId, Long memberId);
 }
