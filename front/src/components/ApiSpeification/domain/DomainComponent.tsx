9731fbfcf80f7ccf3e01c26cbf8bac97e0379735
doongdang <doongdang@naver.com>
2024-05-03 16:07:55 +0900

    feat: 도메인 수정 및 삭제

--- front/src/components/ApiSpeification/domain/DomainComponent.tsx
+++ front/src/components/ApiSpeification/domain/DomainComponent.tsx
@@ -1,6 +1,23 @@
 import Box from "@mui/material/Box";
 import {Chip} from "@mui/material";
-const DomainComponent = ({domainId, name}: { domainId: number, name: string }) => {
+import {useState} from "react";
+import DomainModal from "./DomainModal.tsx";
+
+
+
+const DomainComponent = (
+    {projectId, apiSpecificationId, domainId, name, onChildChange}:
+        { projectId: number, apiSpecificationId: number, domainId: number, name: string, onChildChange: () => void}
+) => {
+    const [open, setOpen] = useState<boolean>(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const changeOpen = (isOpen: boolean) => {
+        setOpen(isOpen)
+    }
 
     return (
         <Box
@@ -11,8 +28,18 @@ const DomainComponent = ({domainId, name}: { domainId: number, name: string }) =
             <Chip
                 label={name}
                 variant="outlined"
+                onClick={handleOpen}
             />
-            <Chip label={domainId} variant="outlined"/>
+            <DomainModal
+                projectId={projectId}
+                apiSpecificationId={apiSpecificationId}
+                domainId={domainId}
+                name={name}
+                open={open}
+                onChildChange={onChildChange}
+                changeOpen={changeOpen}
+            />
+
         </Box>
     )
 }
