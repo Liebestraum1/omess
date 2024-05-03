804a96dfe280892e2d0e02b3d5177a74aa7c0b37
chlwodyd9829 <chlwodyd9829@naver.com>
2024-05-03 12:40:07 +0900

    chore: 채팅 메시지 구분에 따라 생성 및 응답 로직 추가

--- omess-chat/src/main/java/com/sixback/omesschat/domain/chat/mapper/ChatMessageMapper.java
+++ omess-chat/src/main/java/com/sixback/omesschat/domain/chat/mapper/ChatMessageMapper.java
@@ -1,24 +1,33 @@
 package com.sixback.omesschat.domain.chat.mapper;
 
+import com.sixback.omesschat.domain.chat.formatter.MessageFormatter;
 import com.sixback.omesschat.domain.chat.model.dto.response.ChatMessageDto;
 import com.sixback.omesschat.domain.chat.model.entity.ChatMessage;
 import com.sixback.omesschat.domain.member.model.dto.MemberInfo;
 
-public class ChatMessageMapper {
+import static com.sixback.omesschat.domain.chat.model.entity.MessageType.SYSTEM;
+import static com.sixback.omesschat.domain.chat.model.entity.MessageType.USER;
 
+public class ChatMessageMapper {
 
     public static ChatMessageDto toResponse(ChatMessage message, MemberInfo member) {
         return new ChatMessageDto(
+                message.getClassify(),
                 member,
                 message.getId(),
-                message.getMessage(),
+                MessageFormatter.ofType(message.getClassify(), message.getMessage(), member.getNickname()),
                 message.getCreateAt(),
                 message.isUpdated(),
                 message.isPined()
         );
     }
 
-    public static ChatMessage to(String chatId, Long memberId, String message) {
-        return new ChatMessage(chatId, memberId, message);
+    public static ChatMessage toUserMessage(String chatId, Long memberId, String message) {
+        return new ChatMessage(USER, chatId, memberId, message);
     }
+
+    public static ChatMessage toSystemMessage(String chatId, Long memberId, String message) {
+        return new ChatMessage(SYSTEM, chatId, memberId, message);
+    }
+
 }
