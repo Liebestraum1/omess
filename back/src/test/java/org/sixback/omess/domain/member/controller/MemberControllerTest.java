27585810660ef91ea04be0b25e48cd064ebddb26
bndgc@naver.com <bndgc@naver.com>
2024-05-03 17:25:02 +0900

    test: member-signout 기능 테스트

--- back/src/test/java/org/sixback/omess/domain/member/controller/MemberControllerTest.java
+++ back/src/test/java/org/sixback/omess/domain/member/controller/MemberControllerTest.java
@@ -1,6 +1,7 @@
 package org.sixback.omess.domain.member.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -8,7 +9,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.sixback.omess.domain.member.exception.MemberErrorMessage;
 import org.sixback.omess.domain.member.model.dto.request.SignInMemberRequest;
 import org.sixback.omess.domain.member.model.dto.request.SignupMemberRequest;
 import org.sixback.omess.domain.member.model.entity.Member;
@@ -16,6 +16,7 @@ import org.sixback.omess.domain.member.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,6 +24,8 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static org.sixback.omess.common.TestUtils.makeMember;
 import static org.sixback.omess.common.exception.ErrorType.*;
+import static org.sixback.omess.domain.member.exception.MemberErrorMessage.DUPLICATE_EMAIL;
+import static org.sixback.omess.domain.member.exception.MemberErrorMessage.DUPLICATE_NICKNAME;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -220,14 +223,14 @@ class MemberControllerTest {
         }
 
         @Test
-        @DisplayName("회원가입 - 실패")
-        void test() throws Exception {
+        @DisplayName("회원가입 - 이미 존재하는 이메일으로 인한 실패")
+        void signup_exist_email_fail() throws Exception {
             // given
             Member havingExistEmailMember = makeMember("nickname", "email@naver.com", "password123");
             memberRepository.save(havingExistEmailMember);
 
             String signupMemberRequest = objectMapper.writeValueAsString(new SignupMemberRequest(
-                    "nickname", "email@naver.com", "password"));
+                    "nickname1", "email@naver.com", "password"));
 
             // when
             mockMvc.perform(post("/api/v1/members/signup")
@@ -235,7 +238,30 @@ class MemberControllerTest {
                             .content(signupMemberRequest))
                     //then
                     .andExpect(status().isBadRequest())
-                    .andExpect(jsonPath("$.title").value(MemberErrorMessage.DUPLICATE_EMAIL.getMessage()))
+                    .andExpect(jsonPath("$.title").value(DUPLICATE_EMAIL.getMessage()))
+                    .andExpect(jsonPath("$.status").value(BAD_REQUEST.value()))
+                    .andExpect(jsonPath("$.instance").value("/api/v1/members/signup"))
+                    .andDo(print())
+            ;
+        }
+
+        @Test
+        @DisplayName("회원가입 - 이미 존재하는 닉네임으로 인한 실패")
+        void signup_exist_nickname_fail() throws Exception {
+            // given
+            Member havingExistNicknameMember = makeMember("nickname", "email@naver.com", "password123");
+            memberRepository.save(havingExistNicknameMember);
+
+            String signupMemberRequest = objectMapper.writeValueAsString(new SignupMemberRequest(
+                    "nickname", "email1@naver.com", "password"));
+
+            // when
+            mockMvc.perform(post("/api/v1/members/signup")
+                            .contentType(APPLICATION_JSON)
+                            .content(signupMemberRequest))
+                    //then
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.title").value(DUPLICATE_NICKNAME.getMessage()))
                     .andExpect(jsonPath("$.status").value(BAD_REQUEST.value()))
                     .andExpect(jsonPath("$.instance").value("/api/v1/members/signup"))
                     .andDo(print())
@@ -313,5 +339,47 @@ class MemberControllerTest {
                     .andExpect(jsonPath("$.instance").value("/api/v1/members/signin"))
                     .andDo(print());
         }
+    }
+
+    @Nested
+    class SignoutTest {
+        @Test
+        @DisplayName("로그아웃 - 성공")
+        void signin_success() throws Exception {
+            Member member = makeMember("nickname", "e@naver.com", "password123");
+            memberRepository.save(member);
+            Cookie cookie = getCookie("e@naver.com", "password123");
+
+            // when
+            mockMvc.perform(post("/api/v1/members/signout")
+                            .contentType(APPLICATION_JSON)
+                            .cookie(cookie))
+                    //then
+                    .andExpect(status().isOk())
+                    .andDo(print());
+        }
+
+        @Test
+        @DisplayName("로그아웃 - 로그인하지 않았던 사용자가 로그아웃 요청 실패")
+        void signin_fail_notSignin() throws Exception {
+            // when
+            mockMvc.perform(post("/api/v1/members/signout")
+                            .contentType(APPLICATION_JSON))
+                    //then
+                    .andExpect(status().isUnauthorized())
+                    .andExpect(jsonPath("$.title").value(NEED_AUTHENTICATION_ERROR.getTitle()))
+                    .andExpect(jsonPath("$.status").value(UNAUTHORIZED.value()))
+                    .andExpect(jsonPath("$.instance").value("/api/v1/members/signout"))
+                    .andDo(print());
+        }
+    }
+
+    private Cookie getCookie(String email, String password) throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(post("/api/v1/members/signin")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(new SignInMemberRequest(email, password))))
+                .andReturn()
+                .getResponse();
+        return response.getCookie("SESSION");
     }
 }
