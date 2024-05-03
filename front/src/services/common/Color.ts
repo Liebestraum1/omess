943226cbb10cd3b04bff9ab0e1239118b6cb4858
jsy6265 <71249591+jsy6265@users.noreply.github.com>
2024-05-03 17:38:12 +0900

    feat : add Color Array

---
+++ front/src/services/common/Color.ts
@@ -0,0 +1,22 @@
+export const ColorArr = [
+    "#FADEC9",
+    "#EEE0DA",
+    "#FFF0E1",
+    "#E6D6F0",
+    "#FDECC8",
+    "#FFDFD0",
+    "#DFE8EE",
+    "#FDECC8",
+    "#E8DEEE",
+    "#E8DEEE",
+    "#E8DEEE",
+    "#FDECC8",
+    "#E9E1D9",
+    "#FBE9D1",
+    "#EEDFEC",
+    "#F7D8C4",
+    "#E7E1EF",
+    "#FFEBE0",
+    "#DDEADF",
+    "#FCEFE8"
+];
