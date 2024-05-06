a461c64b29a7357e7c33582324972fd8236bdb7a
j-yong98 <chlwodyd9829@gmail.com>
2024-05-06 22:43:07 +0900

    move: dto/request -> dto/request/message

--- omess-chat/src/main/java/com/sixback/omesschat/domain/chat/model/dto/request/message/RequestType.java
+++ omess-chat/src/main/java/com/sixback/omesschat/domain/chat/model/dto/request/message/RequestType.java
@@ -1,4 +1,4 @@
-package com.sixback.omesschat.domain.chat.model.dto.request;
+package com.sixback.omesschat.domain.chat.model.dto.request.message;
 
 public enum RequestType {
     ENTER, SEND, UPDATE, DELETE, LOAD, PIN, HEADER, CHAT_NAME
