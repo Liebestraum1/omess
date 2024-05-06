4603ba1c682daf419efd4e6132ac800c0d2ca7c1
j-yong98 <chlwodyd9829@gmail.com>
2024-05-06 22:45:10 +0900

    feat: 이메일로 회원 조회

--- omess-chat/src/main/java/com/sixback/omesschat/domain/member/repository/MemberRepository.java
+++ omess-chat/src/main/java/com/sixback/omesschat/domain/member/repository/MemberRepository.java
@@ -2,6 +2,9 @@ package com.sixback.omesschat.domain.member.repository;
 
 import com.sixback.omesschat.domain.member.model.entity.Member;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 public interface MemberRepository extends ReactiveCrudRepository<Member, Long> {
+
+    Mono<Member> findByEmail(String email);
 }
