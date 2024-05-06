907f35eea731d556c4ae9863ee9197bfa3abe905
j-yong98 <chlwodyd9829@gmail.com>
2024-05-06 22:46:24 +0900

    add: findByProjectIdAndMemberId, findByChatIdAndMemberId

--- omess-chat/src/main/java/com/sixback/omesschat/domain/chat/repository/ChatRepository.java
+++ omess-chat/src/main/java/com/sixback/omesschat/domain/chat/repository/ChatRepository.java
@@ -1,7 +1,16 @@
 package com.sixback.omesschat.domain.chat.repository;
 
 import com.sixback.omesschat.domain.chat.model.entity.Chat;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface ChatRepository extends ReactiveMongoRepository<Chat, String> {
+
+    @Query("{projectId: ?0, members: {memberId: ?1, isAlive: true}}")
+    Flux<Chat> findByProjectIdAndMemberId(Long projectId, Long memberId);
+
+    @Query("{id: ?0, members:  {memberId: ?1, isAlive: true}}")
+    Mono<Chat> findByChatIdAndMemberId(String chatId, Long memberId);
 }
