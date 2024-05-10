b7f76b1aacc9cbf14d54cde604bf3585be7b50f9
bndgc@naver.com <bndgc@naver.com>
2024-05-10 22:26:23 +0900

    feat: get members in project 기능

--- back/src/main/java/org/sixback/omess/domain/project/mapper/ProjectMapper.java
+++ back/src/main/java/org/sixback/omess/domain/project/mapper/ProjectMapper.java
@@ -1,11 +1,13 @@
 package org.sixback.omess.domain.project.mapper;
 
+import org.sixback.omess.domain.member.model.entity.Member;
 import org.sixback.omess.domain.project.dto.response.GetProjectMemberResponse;
 import org.sixback.omess.domain.project.dto.response.GetProjectMembersResponse;
 import org.sixback.omess.domain.project.model.dto.request.CreateProjectRequest;
 import org.sixback.omess.domain.project.model.dto.response.CreateProjectResponse;
 import org.sixback.omess.domain.project.model.entity.Project;
 import org.sixback.omess.domain.project.model.entity.ProjectMember;
+import org.sixback.omess.domain.project.model.response.GetMembersInProjectResponse;
 
 import java.util.List;
 
@@ -30,5 +32,13 @@ public class ProjectMapper {
 
     public static GetProjectMemberResponse toGetProjectResponse(ProjectMember projectMember) {
         return new GetProjectMemberResponse(projectMember.getProject().getId(), projectMember.getProject().getName());
+    }
+
+    public static GetMembersInProjectResponse toGetMembersInProjectResponse(ProjectMember projectMember) {
+        Member member = projectMember.getMember();
+        if (member == null) {
+            return null;
+        }
+        return new GetMembersInProjectResponse(member.getId(), member.getNickname(), member.getProfile());
     }
 }
