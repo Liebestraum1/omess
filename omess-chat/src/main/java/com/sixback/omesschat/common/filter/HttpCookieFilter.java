3ff48fd3acfb296d952cde1cbe8c79189f891518
j-yong98 <chlwodyd9829@gmail.com>
2024-05-11 21:45:14 +0900

    add: cors 필터

--- omess-chat/src/main/java/com/sixback/omesschat/common/filter/HttpCookieFilter.java
+++ omess-chat/src/main/java/com/sixback/omesschat/common/filter/HttpCookieFilter.java
@@ -2,18 +2,21 @@ package com.sixback.omesschat.common.filter;
 
 import com.sixback.omesschat.common.utils.Base64Util;
 import com.sixback.omesschat.domain.session.repository.SessionRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpCookie;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
-import java.util.Optional;
-
+@Slf4j
 @RequiredArgsConstructor
 public class HttpCookieFilter implements WebFilter, Ordered {
 
@@ -24,6 +27,12 @@ public class HttpCookieFilter implements WebFilter, Ordered {
         ServerHttpRequest request = exchange.getRequest();
 
         MultiValueMap<String, HttpCookie> cookies = request.getCookies();
+
+        if (cookies.isEmpty()) {
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
         String session = Optional.ofNullable(cookies.get("SESSION")).orElseThrow().getFirst().getValue();
         String sessionId = Base64Util.decode(session);
 
@@ -35,6 +44,6 @@ public class HttpCookieFilter implements WebFilter, Ordered {
 
     @Override
     public int getOrder() {
-        return Ordered.HIGHEST_PRECEDENCE;
+        return -2;
     }
 }
