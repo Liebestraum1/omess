9f29cc5bd394b02dce53cccf37e42a8459585adb
j-yong98 <chlwodyd9829@gmail.com>
2024-05-12 20:50:27 +0900

    chore: 채팅 websocket config

--- omess-chat/src/main/java/com/sixback/omesschat/common/config/WebConfig.java
+++ omess-chat/src/main/java/com/sixback/omesschat/common/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.sixback.omesschat.common.config;
 import com.sixback.omesschat.common.filter.CorsFilter;
 import com.sixback.omesschat.common.filter.HttpCookieFilter;
 import com.sixback.omesschat.common.filter.WebSocketUpgradeFilter;
+import com.sixback.omesschat.domain.chat.service.WebSocketSessionService;
 import com.sixback.omesschat.domain.session.repository.SessionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,7 @@ import org.springframework.web.server.WebFilter;
 public class WebConfig {
 
     private final DatabaseClient databaseClient;
+    private final WebSocketSessionService webSocketSessionService;
 
     @Bean
     public WebFilter httpCookieFilter() {
@@ -23,7 +25,7 @@ public class WebConfig {
 
     @Bean
     public WebFilter webSocketUpgradeFilter() {
-        return new WebSocketUpgradeFilter();
+        return new WebSocketUpgradeFilter(webSocketSessionService);
     }
 
     @Bean
--- omess-chat/src/main/java/com/sixback/omesschat/common/filter/WebSocketUpgradeFilter.java
+++ omess-chat/src/main/java/com/sixback/omesschat/common/filter/WebSocketUpgradeFilter.java
@@ -1,13 +1,12 @@
 package com.sixback.omesschat.common.filter;
 
+import com.sixback.omesschat.domain.chat.service.WebSocketSessionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
-import org.springframework.http.HttpCookie;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
@@ -16,11 +15,21 @@ import reactor.core.publisher.Mono;
 @Slf4j
 @RequiredArgsConstructor
 public class WebSocketUpgradeFilter implements WebFilter, Ordered {
+
+    private final WebSocketSessionService webSocketSessionService;
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
 
+        Long memberId = exchange.getAttribute("memberId");
         String upgradeHeader = request.getHeaders().getFirst("Upgrade");
+
+        if (memberId == null && upgradeHeader == null) {
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+
         if ("websocket".equalsIgnoreCase(upgradeHeader)) {
             System.out.println("Handling WebSocket upgrade request");
         }
