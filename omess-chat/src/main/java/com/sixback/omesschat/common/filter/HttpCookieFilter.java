81f2582967f030e76adce0b0df17895a95d93c68
chlwodyd9829 <chlwodyd9829@naver.com>
2024-05-13 10:07:47 +0900

    fix: cookie & websocket upgrade filter

--- omess-chat/src/main/java/com/sixback/omesschat/common/filter/HttpCookieFilter.java
+++ omess-chat/src/main/java/com/sixback/omesschat/common/filter/HttpCookieFilter.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpCookie;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
@@ -24,14 +26,18 @@ public class HttpCookieFilter implements WebFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
+        String path = request.getURI().getPath();
+
+        if (path.startsWith("/actuator")) {
+            return chain.filter(exchange);
+        }
 
         MultiValueMap<String, HttpCookie> cookies = request.getCookies();
 
         if (cookies.isEmpty()) {
-//            ServerHttpResponse response = exchange.getResponse();
-//            response.setStatusCode(HttpStatus.UNAUTHORIZED);
-//            return response.setComplete();
-            return chain.filter(exchange);
+            ServerHttpResponse response = exchange.getResponse();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
         }
         String session = Optional.ofNullable(cookies.get("SESSION")).orElseThrow().getFirst().getValue();
         String sessionId = Base64Util.decode(session);
