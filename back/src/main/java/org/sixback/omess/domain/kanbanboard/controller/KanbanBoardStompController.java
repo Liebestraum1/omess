4af046d36c50b41784a75b51fc1e5fed02f922b4
jsy6265 <71249591+jsy6265@users.noreply.github.com>
2024-05-13 15:15:11 +0900

    feat : update stomp response

--- back/src/main/java/org/sixback/omess/domain/kanbanboard/controller/KanbanBoardStompController.java
+++ back/src/main/java/org/sixback/omess/domain/kanbanboard/controller/KanbanBoardStompController.java
@@ -2,6 +2,9 @@ package org.sixback.omess.domain.kanbanboard.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.sixback.omess.domain.kanbanboard.model.dto.request.issue.WriteIssueRequest;
+import org.sixback.omess.domain.kanbanboard.model.dto.response.label.GetLabelResponses;
+import org.sixback.omess.domain.kanbanboard.service.KanbanBoardService;
+import org.sixback.omess.domain.member.model.dto.response.GetIssueResponses;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -10,16 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class KanbanBoardStompController {
+    private final KanbanBoardService kanbanBoardService;
 
-    @MessageMapping("/kanbanboards/{module_id}")
-    @SendTo("/sub/kanbanRoom/{module_id}")
-    public String updateIssue(@DestinationVariable("module_id") Long moduleId) {
-        return moduleId.toString();
+    @MessageMapping("/projects/{project_id}/kanbanboards/{module_id}")
+    @SendTo("/sub/projects/{project_id}/kanbanRoom/{module_id}")
+    public GetIssueResponses updateIssue(@DestinationVariable("project_id") Long projectId,
+                                         @DestinationVariable("module_id") Long moduleId) {
+        GetIssueResponses getIssueResponses = kanbanBoardService.getIssuesAll(projectId, moduleId);
+
+        return getIssueResponses;
     }
 
-    @MessageMapping("/kanbanboards/{module_id}/label")
-    @SendTo("/sub/kanbanRoom/{module_id}/label")
-    public String updateLabel(@DestinationVariable("module_id") Long moduleId) {
-        return moduleId.toString();
+    @MessageMapping("/projects/{project_id}/kanbanboards/{module_id}/label")
+    @SendTo("/sub/projects/{project_id}/kanbanRoom/{module_id}/label")
+    public GetLabelResponses updateLabel(@DestinationVariable("project_id") Long projectId,
+                                         @DestinationVariable("module_id") Long moduleId) {
+        return kanbanBoardService.getLabels(0L, projectId, moduleId);
     }
 }
