f87dbd19319c6e69655971faa6ee8cfb1cbc83a9
bndgc@naver.com <bndgc@naver.com>
2024-05-14 15:34:09 +0900

    feat: file preview & download 기능

--- back/src/main/java/org/sixback/omess/domain/file/mapper/FileMapper.java
+++ back/src/main/java/org/sixback/omess/domain/file/mapper/FileMapper.java
@@ -1,9 +1,19 @@
 package org.sixback.omess.domain.file.mapper;
 
+import org.sixback.omess.domain.file.model.dto.response.GetFileInfoResponse;
 import org.sixback.omess.domain.file.model.dto.response.UploadFileResponse;
 import org.sixback.omess.domain.file.model.entity.FileInformation;
 
 public class FileMapper {
+    public static GetFileInfoResponse toGetFileInfoResponse(FileInformation fileInformation, String address) {
+        return new GetFileInfoResponse(
+                fileInformation.getId(),
+                address,
+                fileInformation.getReferenceType(),
+                fileInformation.getReferenceId()
+        );
+    }
+
     public static UploadFileResponse toUploadFileResponse(FileInformation fileInformation, String address) {
         return UploadFileResponse.builder()
                 .id(fileInformation.getId())
